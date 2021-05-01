af5896b0c1b94db014c39f0c9e21c5a1f7446e75
kmalakhova <kgrekhova@gmail.com>
2021-04-30 18:53:31 -0700

    Update func name

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 planet_bp = Blueprint("planet_bp", __name__)
 
 
-@planet_bp.route("/planets", methods=["GET"])
-def get_planet_info():
+@planet_bp.route("/planets/sass", methods=["GET"])
+def be_sassy():
     return "I'm a planet and you're not."
