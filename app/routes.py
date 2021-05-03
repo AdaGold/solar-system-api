396cf0c003b57046888dedca0de6404278a0efc7
melissa-nguyen <n.melissa.25@gmail.com>
2021-05-03 11:13:05 -0700

    added delete method to planet_id endpoint

--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,7 @@ from app import db
 from app.models.planet import Planet
 from flask import request, Blueprint, make_response, jsonify
 planet_bp = Blueprint("planet", __name__, url_prefix="/planets")
+# endpoint for user to retrieve all planet data & create planet
 @planet_bp.route("", methods=["GET","POST"])
 def handle_planets():
     if request.method == "GET":
@@ -33,11 +34,37 @@ def handle_planets():
         db.session.add(new_planet)
         db.session.commit()
         return make_response(f"Planet {new_planet.name} successfully created", 201)
-@planet_bp.route("/<planet_id>", methods=["GET"])
+
+
+# @planet_bp.route("/<planet_id>", methods=["GET", "DELETE"])
+# def handle_planet(planet_id):
+
+    # planet_id = Planet.query.get(planet_id)
+
+    # if request.method == "GET":
+    #     if planet_id == Planet.query.get(planet_id):
+    #         return {
+    #             "id": planet.id,
+    #             "name": planet.name,
+    #             "description": planet.description
+    #         }
+    #     else:
+    #         return make_response("planet does not exist")
+    
+# endpoint for user to gather planet info based on actual planet id 
+@planet_bp.route("/<planet_id>", methods=["GET", "DELETE"])
 def handle_planet(planet_id):
     planet = Planet.query.get(planet_id)
-    return {
-        "id": planet.id,
-        "name": planet.name,
-        "description": planet.description
-    }
+    # if the request method is GET, return planet id, name & description 
+    if request.method == "GET":
+        return {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description
+        }
+    # if the request method is DELETE, delete the planet with the planet id user has indicated & print success message
+    elif request.method == "DELETE":
+        db.session.delete(planet)
+        db.session.commit()
+        return make_response(f"Planet #{planet.id} successfully deleted")
+
