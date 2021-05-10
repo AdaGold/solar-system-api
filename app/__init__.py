c14d2b88ed7828fef631a5aca9ed3707ed971d97
Lacy Draper <ldrape1@gmail.com>
2021-05-10 12:59:46 -0700

    added load_dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
-
+    
+    if not test_config:
+        #hide a warning about a feature in SQLAlchemy that we wont be using
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        #set app.config['SQLALCHEMY_DATABASE_URI'] to the connection string for our database
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    else:
+        app.config['TESTING'] = True
+        app.config['SQALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
     db.init_app(app)
     migrate.init_app(app,db)
