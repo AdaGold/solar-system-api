ac4c7dc2ae2ea45013e44de97f38c21a58daab4a
dynamicdev3000 <michellemoralesjs@gmail.com>
2021-10-15 14:54:15 -0400

    all planets in, next step one planet1

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,29 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, name, description, moons):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.moons = moons
+
+planets =  [
+    Planet(1, "Mercury", "Mercury is hot, but not too hot for ice", 0),
+    Planet(2, "Mercury", "Venus doesn’t have any moons, and we aren’t sure why", 0),
+    Planet(3, "Earth", "Best planet ever!", 1),
+    Planet(4, "Mars", "Mars has two moons named in Latin that translate to fear and panic", 2),
+]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"])
+def all_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id" : planet.id,
+            "name" : planet.name,
+            "description" : planet.description,
+            "moons" : planet.moons
+        })
+    return jsonify(planets_response)
