76a05516312a4c9d5bc702cf963dc534a751ba05
Monica Cruz <mcatcruz@gmail.com>
2021-10-15 15:59:53 -0400

    Completed list of Planet instances.

--- app/routes.py
+++ app/routes.py
@@ -7,5 +7,14 @@ class Planet:
         self.description = description
         self.number_of_moons = number_of_moons
 
-planets =[Planet(1,'Earth', 'water planet', 1),]
+planets = [
+            Planet(1,'Earth', 'water planet', 1), 
+            Planet(2, 'Mercury', 'fastest', 0),
+            Planet(3, 'Jupiter', 'biggest', 79),
+            Planet(4, 'Saturn', 'gaseous', 82),
+            Planet(5, 'Mars', 'red', 2),
+            Planet(6, 'Uranus', 'coldest', 27),
+            Planet(7, 'Neptune', 'farthest from the sun', 14),
+            Planet(8, 'Venus', 'hottest', 0)
+          ]
 
