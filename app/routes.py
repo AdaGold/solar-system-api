6a3141e3971fb3430854e20e192c0b7fda116d5c
Faith <fkauwe@gmail.com>
2021-10-15 15:02:55 -0700

    Both endpoints created,functioning correctly with Postman

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+planets_bp = Blueprint('planets',__name__, url_prefix='/planets')
 
 class Planet:
     def __init__(self,id, name, description, number_of_moons):
@@ -18,3 +20,18 @@ planets = [
             Planet(8, 'Venus', 'hottest', 0)
           ]
 
+@planets_bp.route('', methods=['GET'])
+def handle_planets():
+  planets_response = []
+  for planet in planets:
+    planets_response.append({'id': planet.id, 'name':planet.name, 
+    'description':planet.description, 'number of moons': planet.number_of_moons})
+  return jsonify(planets_response)
+
+@planets_bp.route('/<planet_id>', methods=['GET'])
+def handle_planet(planet_id):
+  planet_id = int(planet_id)
+  for planet in planets:
+    if planet.id == planet_id:
+      return {'id': planet.id, 'name':planet.name, 
+    'description':planet.description, 'number of moons': planet.number_of_moons}
