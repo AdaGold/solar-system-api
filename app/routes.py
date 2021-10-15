0c55ee35193685fb928b115d70abbf0c0a383fe2
Faith <fkauwe@gmail.com>
2021-10-15 11:21:15 -0700

    class Planet created, 1st planet object created

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,11 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self,id, name, description, number_of_moons):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.number_of_moons = number_of_moons
+
+planets =[Planet(1,'Earth', 'water planet', 1),]
+
