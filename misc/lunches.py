e1816c12646c5b6ce9f309f0ce93db05afdf2f35
karishma <karishmacarterjohnson@gmail.com>
2021-10-16 21:30:55 -0700

    add toy code to understand classes and instances

---
+++ misc/lunches.py
@@ -0,0 +1,11 @@
+class Lunch:
+    def __init__(self, name, food):
+        self.name = name
+        self.food = food
+
+ada_lunches = [
+    Lunch("Karishma", "ramen and eggs"),
+    Lunch("Detroit", "pineapple bowl")
+]
+
+ada_lunches[1].food
