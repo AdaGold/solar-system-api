a8124f2abdb287f7d3eeaee9ff9e670b5def82ce
karishma <karishmacarterjohnson@gmail.com>
2021-10-16 21:35:22 -0700

    add blueprint and endpoint for all planets

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,10 @@ from flask import Blueprint
 
 class Planet:
     def __init__(self, id, name, description, cycle_len):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.cycle_len = cycle_len
+        self.id=id
+        self.name=name
+        self.description=description
+        self.cycle_len=cycle_len # days
 
 planets = [
     Planet(1, "Earth", "blue marble", 365),
@@ -14,3 +14,16 @@ planets = [
     Planet(4, "Mercury", "teeny tiny", 88)
 ]
 
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"])
+def handle_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "cycle length (days)": planet.cycle_len
+        })
+    return jsonify(planets_response)
