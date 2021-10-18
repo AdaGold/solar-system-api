fdb24512234fad2f388955b65ff9c98a56b2c835
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-10-18 14:28:23 -0700

    create planet class

---
+++ app/planets_class.py
@@ -0,0 +1,6 @@
+class Planet:
+    def __init__(self, id, name, description, num_of_moons):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.num_of_moons = num_of_moons
