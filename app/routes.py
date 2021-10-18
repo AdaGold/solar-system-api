14de83cbf5cabbeddbb6bbca46ec451892a617ad
Katie <diazkatie@gmail.com>
2021-10-18 13:34:06 -0700

    Named Planet class and created planet list Co-authored-by: bycreatinganaccountyouagreetothetermsof <bycreatinganaccountyouagreetothetermsof@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,20 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description, color):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.color = color 
+        
+planets = [
+    Planet(1, "mercury", "the littlest planet", "gray"),
+    Planet(2, "venus", "the hottest planet", "maroon"),
+    Planet(3, "earth", "the liveliest planet", "green"),
+    Planet(4, "mars", "the reddest planet", "red" ),
+    Planet(5, "jupiter", "the biggest planet", "orange"),
+    Planet(6, "saturn", "the ring planet", "yellow"),
+    Planet(7, "uranus", "the most sidways planet", "purple"),
+    Planet(8, "neptune", "the boring planet", "blue"),
+]
+
