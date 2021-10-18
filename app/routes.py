f7169b937ad94f9391d50747a492ced034343a9d
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-10-18 12:17:14 -0700

    loading json data as dictionaries

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,8 @@
 from flask import Blueprint
+import load_json
+import pprint
+
+# loads json data as dictionaries
+planet_data = load_json.load('planets.json')
+satellite_data = load_json.load('satellites.json')
 
