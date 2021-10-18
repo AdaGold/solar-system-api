627cacdde7c8d5d9275234a976bf7ef3de0f9473
Areeba Rashid <areeba.r.qureshi@gmail.com>
2021-10-18 14:00:48 -0700

    updated the elements to those of the crust or atmosphere

--- app/routes.py
+++ app/routes.py
@@ -8,16 +8,18 @@ class Planet:
         self.elements = elements
 
 planets = [
-    Planet(1, "Mercury", "First planet from the sun.", "elements"),
-    Planet(2, "Venus", "Second planet from the sun.", "elements"),
-    Planet(3, "Earth", "Third planet from the sun.", "elements"),
-    Planet(4, "Mars", "Fourth planet from the sun.", "elements"),
-    Planet(5, "Jupiter", "Fifth planet from the sun.", "elements"),
-    Planet(6, "Saturn", "Sixth planet from the sun.", "elements"),
-    Planet(7, "Uranus", "Seventh planet from the sun.", "elements"),
-    Planet(8, "Neptune", "Eighth planet from the sun.", "elements"),
-    Planet(9, "Pluto", "Still a planet!", "elements")
+    Planet(1, "Mercury", "First planet from the sun.", "Oxygen, Magnesium, Aluminum, Silicon, Calcium"),
+    Planet(2, "Venus", "Second planet from the sun.", "Carbon, Nitrogen, Oxygen, Sulfur, Argon"),
+    Planet(3, "Earth", "Third planet from the sun.", "Oxygen, Aluminum, Silicon, Calcium, Iron"),
+    Planet(4, "Mars", "Fourth planet from the sun.", "Oxygen, Magnesium, Aluminum, Silicon, Iron"),
+    Planet(5, "Jupiter", "Fifth planet from the sun.", "Hydrogen, Helium, Carbon, Nitrogen, Oxygen"),
+    Planet(6, "Saturn", "Sixth planet from the sun.", "Hydrogen, Helium, Carbon, Nitrogen, Oxygen"),
+    Planet(7, "Uranus", "Seventh planet from the sun.", "Hydrogen, Helium, Carbon, Nitrogen, Sulfur"),
+    Planet(8, "Neptune", "Eighth planet from the sun.", "Hydrogen, Helium, Carbon, Nitrogen, Oxygen"),
+    Planet(9, "Pluto", "Still a planet!", "Hydrogen, Oxygen, Magnesium, Silicon, Iron")
 ]
+
+#https://www.lpi.usra.edu/education/IYPT/
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
