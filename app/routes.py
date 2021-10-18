181215cb8c3cb8627b457d922d8a96881b5e8307
Ana Gabriele <gabbanasoares@gmail.com>
2021-10-18 14:31:16 -0700

    Create Planet class and route to access planets

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,30 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+
+planets_bp = Blueprint("planets", __name__)
+
+class Planet:
+    def __init__(self, name, id, type, description, moons) -> None:
+        self.name = name
+        self.id = id
+        self.type = type
+        self.description = description
+        self.moons = moons
+
+
+planet_1= Planet("Mercury", 1, "Rocky", "Mercury is the closest planet to the sun and the eighth largest. It has a diameter of 4,880 kilometers. ", "1 Moon")
+planet_2= Planet("Venus", 2, "Terrestrial","Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.", "13 Moons")
+
+
+planet_lst=[planet_1,planet_2]
+@planets_bp.route("/planets", methods =["GET"])
+
+def handle_planets():
+    planets_response=[]
+    for planet in planet_lst:
+        planets_response.append({"id": planet.id, 
+        "name": planet.name,
+        "type": planet.type,
+        "description": planet.description,
+        "moons": planet.moons})
+    return jsonify(planets_response)
