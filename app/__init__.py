84885001fae26e3a5bd1e1638973cd075586aab0
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-10-19 11:15:39 -0700

    registering satellite app

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,8 @@ def create_app(test_config=None):
 
     from .routes.planet_routes import planets_bp
     app.register_blueprint(planets_bp)
+
+    from .routes.satellite_routes import satellite_bp
+    app.register_blueprint(satellite_bp)
     
     return app
