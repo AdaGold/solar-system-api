e944b910fb4683be0d5a3eebbc19dd609ced4512
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-10-19 11:13:19 -0700

    satellite class added

---
+++ app/satellites_class.py
@@ -0,0 +1,6 @@
+class Satellite:
+    def __init__(self, name, id, planet_id, description):
+        self.name = name
+        self.id = id
+        self.planet_id = planet_id
+        self.description = description
