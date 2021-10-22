dc1b0fcece651b999016194ea449ead39c2cf1ff
syblades <86990570+syblades@users.noreply.github.com>
2021-10-21 23:15:38 -0700

    removed hardcoded Planet data

--- app/routes.py
+++ app/routes.py
@@ -1,47 +1,40 @@
-from flask import Blueprint
-from flask import Blueprint, jsonify 
-
-
-class Planet:
-    def __init__(self, id, name, description, color = None):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.color = color
-
-
-planets = [
-    Planet(1, "Mercury", "Smallest planet in our solar system.", "light grey"),
-    Planet(2, "Mars", " A planet with a very thin atmosphere made of carbon dioxide, nitrogen, and argon.",["red", "brown", "tan"]),
-    Planet(3, "Venus", "The brighest object in the sky second to the Sun and Moon.", ["brown", "grey"]),
-    Planet(4, "Earth", "What we live on!", ["blue", "brown-green", "white"]),
-    Planet(5, "Jupiter", "The largest planet in our solar system", ["white", "red", "brown", "yellow"]),
-    Planet(6, "Saturn", "Is comprised of thousands of ringlets made of chunks of ice and rock", ["yellow", "brown", "red", "white"]),
-    Planet(7, "Uranus", "Rotates at nearly a 90 degree angle from the plane of its orbit", ["blue", "green"]),
-    Planet(8, "Neptune", "First planet located through mathematical calulations", ["white", "red", "blue", "yellow"])
-]
+from flask import Blueprint, jsonify, make_response, request
+from app import db
+from app.models.planet import Planet
 
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-# client can request all planets listed in "list(planets)"
-@planets_bp.route("", methods=["GET"])
+
+@planets_bp.route("", methods=["GET", "POST"])
 def find_planets():
-    planets_response = []
-    for planet in planets:
-        planets_response.append({
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "color": planet.color
-        })
-    return jsonify(planets_response)
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "color": planet.color
+            })
+        return jsonify(planets_response)
+
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"], description=request_body["description"], color=request_body["color"])
+
+        db.session.add(new_planet)
+        db.session.commit()
+
+        return make_response(f"{new_planet.name} has been successfully created", 201)
 
 
-# client can request a specific planet listed in "list(planets)"
+
 @planets_bp.route("/<planet_id>", methods=["GET"])
-def find_one_planet(planet_id):
+def find_planet(planet_id):
     planet_id = int(planet_id)
+    planets = Planet.query.all()
     for planet in planets:
         if planet_id == planet.id:
             return {
@@ -50,3 +43,4 @@ def find_one_planet(planet_id):
                 "description": planet.description,
                 "color": planet.color
             }
+
