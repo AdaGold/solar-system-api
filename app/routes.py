9f06150c633e8227194346d51dbabd322a33ccfa
Shaina Beth <shainabeth94@gmail.com>
2021-10-22 10:54:53 -0700

    created Planets class

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,8 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description, moons):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.moons = moons 
