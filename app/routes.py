65298d079d847eecc25d42161842ef58ba2c3053
Shaina Beth <shainabeth94@gmail.com>
2021-10-22 10:58:52 -0700

    created routes

--- app/routes.py
+++ app/routes.py
@@ -13,3 +13,26 @@ planets = [
     Planet(3, "Jupiter", "Gas giant", "53")
     ]
 planets_bp = Blueprint("planets", __name__,url_prefix="/planets")
+
+@planets_bp.route("", methods = ["GET"])
+def get_planets():
+    planet_response = []
+    for planet in planets:
+        planet_response.append({
+            "id" : planet.id,
+            "name" : planet.name,
+            "description" : planet.description,
+            "color" : planet.color,
+        })
+    return jsonify(planet_response)
+@planets_bp.route("/<planet_id>", methods = ["GET"])
+def get_single_planet(planet_id):
+    given_planet_id = int(planet_id)
+    for planet in planets:
+        if planet.id == given_planet_id:
+            return {
+                "id" : planet.id,
+                "name" : planet.name,
+                "description" : planet.description,
+                "moons" : planet.color,
+                }
