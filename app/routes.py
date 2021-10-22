aac9e2c59c21df597f856ee11f09472d25c19523
Shaina Beth <shainabeth94@gmail.com>
2021-10-22 10:56:33 -0700

    created list of planets and a planets blueprint

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,10 @@ class Planet:
         self.name = name
         self.description = description
         self.moons = moons 
+
+planets = [
+    Planet(1, "Mercury", "Smallest planet", "None"),
+    Planet(2,"Venus", "Second planet from sun","None"),
+    Planet(3, "Jupiter", "Gas giant", "53")
+    ]
+planets_bp = Blueprint("planets", __name__,url_prefix="/planets")
