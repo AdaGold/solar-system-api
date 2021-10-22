6c3101f4fd2f63042f9839a869332425d4006f40
Mary <marytian.mt@gmail.com>
2021-10-22 12:21:13 -0600

    Created database. Set up Planet model.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-# #test_config param needed?
-# def create_app(test_config=None):
-def create_app():
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(test_config=None):
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    # Import models here
+    from app.models.planet import Planet
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register Blueprints here
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
+
     return app
