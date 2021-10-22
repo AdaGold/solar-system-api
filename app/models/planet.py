f9a4790987605a5ab065a9264cb35091a7a5d895
kristina <kkraft423@gmail.com>
2021-10-22 14:27:11 -0400

    whitespace deleted and handle one planet refactored

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,7 +1,7 @@
 from app import db
 
 class Planet(db.Model):
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
     description = db.Column(db.String)
     num_of_moons = db.Column(db.Integer)
--- app/routes.py
+++ app/routes.py
@@ -2,21 +2,6 @@ from flask import Blueprint, jsonify, request, make_response
 from app import db
 from app.models.planet import Planet
 
-class Planet:
-    def __init__(self, id, name, description, num_of_moons):
-        self.id = id 
-        self.name = name 
-        self.description = description 
-        self.num_of_moons = num_of_moons
-
-# planets = [
-#     Planet(1, "Mercury", "First planet from the sun", 0),
-#     Planet(2, "Venus", "Second planet from the sun", 0),
-#     Planet(3, "Earth", "Third planet from the sun", 1),
-#     Planet(4, "Mars", "Fourth planet from the sun", 2),
-#     Planet(5, "Jupiter", "Fifth planet from the sun", 79)
-# ]
-
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
 def make_planet_dict(planet):
@@ -42,8 +27,8 @@ def handle_planets():
     else: 
         request_body = request.get_json()
         new_planet = Planet(
-            name = request_body["name"],
-            description= request_body["description"],
+            name=request_body["name"],
+            description=request_body["description"],
             num_of_moons=request_body["num_of_moons"]
         )
         db.session.add(new_planet)
@@ -53,6 +38,9 @@ def handle_planets():
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def handle_one_planet(planet_id):
+    
+    planets = Planet.query.all()
+
     planet_response = jsonify("Not a valid planet")
 
     for planet in planets:
