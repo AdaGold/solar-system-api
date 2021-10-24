728674f07d4a77219a6028cd2b1e7e5e2433c9c6
Angela <angelfan123@gmail.com>
2021-10-24 14:35:21 -0700

    craete solar_system_development in postgres and setup init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,31 @@
+# from flask import Flask
+
+
+# def create_app(test_config=None):
+#     app = Flask(__name__)
+
+#     from .routes import planets_bp
+#     app.register_blueprint(planets_bp)
+
+#     return app
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
-def create_app(test_config=None):
+def create_app(test_config = None):
     app = Flask(__name__)
+    
+    app.config['SQLAlCHEMY_TRACK_MODIFICATIONS']= False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.models.planet import Planet
 
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
