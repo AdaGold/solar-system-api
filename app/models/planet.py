3d68c5002d62b3aac7e22a214cb6921cfd09bb82
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-10-25 14:21:15 -0700

    removed nullable, might put back

--- app/models/planet.py
+++ app/models/planet.py
@@ -2,6 +2,6 @@ from app import db
 
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    num_of_moons = db.Column(db.String, nullable=False)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    num_of_moons = db.Column(db.String)
