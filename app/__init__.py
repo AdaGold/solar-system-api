c872eb6bfb38b7549cee92c407ec580bf043ec06
Lilly <waterlilly169@GMAIL.COM>
2021-10-25 13:27:18 -0700

    added model, added planet class, connected app to database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLAlCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
     
+    from app.Model.planet import Planet
+
     return app
