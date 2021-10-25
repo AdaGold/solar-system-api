a8befa87d5477b71ce9fa9231b7072a91ff7611f
lain <lain.winslow@gmail.com>
2021-10-25 14:48:27 -0700

    added routes for part 2

--- app/__init__.py
+++ app/__init__.py
@@ -5,18 +5,21 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 migrate = Migrate()
 
+
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/planets_database'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "postgresql+psycopg2://postgres:postgres@localhost:5432/planets_database"
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from .routes import planets_bp, one_planet_bp
-    app.register_blueprint(planets_bp)
-    app.register_blueprint(one_planet_bp)
+    from .routes import planet_bp
+
+    app.register_blueprint(planet_bp)
 
     from app.models.planet import Planet
 
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,6 @@
-from flask import Blueprint, jsonify
-
-
-#class Planet:
-#    def __init__(self, id, name, description, color):
-#        self.id = id
-#        self.name = name
-#        self.description = description
-#        self.color = color
-
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, make_response, request
 
 # planets = [
 #     Planet(1, "mercury", "the littlest planet", "gray"),
@@ -20,12 +13,39 @@ from flask import Blueprint, jsonify
 #     Planet(8, "neptune", "the boring planet", "blue"),
 # ]
 
-planets_bp = Blueprint("planets", __name__)
-one_planet_bp = Blueprint("one_planet", __name__, url_prefix="/planet")
+planet_bp = Blueprint("planet", __name__, url_prefix="/planets")
 
 
-#@planets_bp.route("/planets", methods=["GET"])
-#def all_planets():
+@planet_bp.route("", methods=["GET", "POST"])
+def handle_planets():
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append(planet.to_json())
+        return jsonify(planets_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(
+            name=request_body["name"],
+            description=request_body["description"],
+            color=request_body["color"],
+        )
+
+        db.session.add(new_planet)
+        db.session.commit()
+
+        return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+
+# localhost:5000/planets/<planet_id>
+@planet_bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    if request.method == "GET":
+        planet = Planet.query.get(planet_id)
+        return planet.to_json()
+
+
 #    planets_return = []
 #    for planet in planets:
 #        planets_return.append(planet.to_json())
@@ -34,8 +54,8 @@ one_planet_bp = Blueprint("one_planet", __name__, url_prefix="/planet")
 
 # one_planet_bp = Blueprint("one_planet", __name__, url_prefix="/planet")
 # http://127.0.0.1:5000/planet/<planet_id>
-#@one_planet_bp.route("/<planet_id>", methods=["GET"])
-#def one_planet(planet_id):
+# @one_planet_bp.route("/<planet_id>", methods=["GET"])
+# def one_planet(planet_id):
 #    planet_id = int(planet_id)
 #    for planet in planets:
 #        if planet.id == planet_id:
