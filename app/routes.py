f6694ce20ae3c4fc948997000a4a37ea591bb3dc
karishma <karishmacarterjohnson@gmail.com>
2021-10-26 11:12:40 -0700

    remove comments

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,7 @@
 from app import db
 from app.models.planet import Planet
 from flask import Blueprint, jsonify, make_response, request
-'''
-class Planet:
-    def __init__(self, id, name, description, cycle_len):
-        self.id=id
-        self.name=name
-        self.description=description
-        self.cycle_len=cycle_len # days
 
-planets = [
-    Planet(1, "Earth", "blue marble", 365),
-    Planet(2, "Saturn", "ringed planet", 10220),
-    Planet(3, "Mars", "musty, dusty and cold", 780),
-    Planet(4, "Mercury", "teeny tiny", 88)
-]
-
-'''
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 @planets_bp.route("", methods=["GET", "POST"])
