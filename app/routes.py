86a688c17d3e97f553ef2230087c7a5ad6a48dd9
dynamicdev3000 <michellemoralesjs@gmail.com>
2021-10-26 18:56:08 -0400

    added some routes and guard clauses

--- app/routes.py
+++ app/routes.py
@@ -1,60 +1,32 @@
-from flask import Blueprint, jsonify
-import requests
-
-# class Planet:
-#     def __init__(self, id, name, description, moons):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.moons = moons
-
-# planets =  [
-#     Planet(1, "Mercury", "Mercury is hot, but not too hot for ice", 0),
-#     Planet(2, "Mercury", "Venus doesn’t have any moons, and we aren’t sure why", 0),
-#     Planet(3, "Earth", "Best planet ever!", 1),
-#     Planet(4, "Mars", "Mars has two moons named in Latin that translate to fear and panic", 2),
-# ]
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, request
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-# @planets_bp.route("", methods=["GET"])
-# def all_planets():
-#     planets_response = []
-#     for planet in planets:
-#         planets_response.append({
-#             "id" : planet.id,
-#             "name" : planet.name,
-#             "description" : planet.description,
-#             "moons" : planet.moons
-#         })
-#     return jsonify(planets_response)
+@planets_bp.route("", methods=["GET", "POST"])
+def handle_planets():
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id" : planet.id,
+                "name" : planet.name,
+                "description" : planet.description,
+                "moons" : planet.moons
+            })
+        return jsonify(planets_response)
+    
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"],
+                        description=request_body["description"])
 
-# @planets_bp.route("/category_planets", methods=["GET"])
-# def get_category_planets(): 
-#     path = "https://api.le-systeme-solaire.net/rest.php/bodies"
+        db.session.add(new_planet)
+        db.session.commit()
 
-#     query_params = {
-#         "filter[]": "isPlanet,neq,false",
-#         "data": "id,englishName,isPlanet"
-#     }
+        return (f"Planet {new_planet.name} successfully created", 201)
 
-#     response = requests.get(path, params=query_params)
-
-#     return response.json()
-
-# @planets_bp.route("/<planet_id>", methods=["GET"])
-# def handle_planet(planet_id):
-#     planet_id = int(planet_id)
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return {
-#                "id" : planet.id,
-#                 "name" : planet.name,
-#                 "description" : planet.description,
-#                 "moons" : planet.moons
-#             }
-
-
-
-    # print("The value of response is", response)
-    # print("The value of response.text, which contains a text description of the request, is", response.text)
+# @planets_bp.route("/<planet_id>", methods=["GET", "PUT", "DELETE"])
+# https://github.com/DynamicDev3000/hello-books-api/blob/07-update-and-delete/app/routes.py
