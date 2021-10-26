492afe666f78523b3656e26b65e95997a0dfc83f
lain <lain.winslow@gmail.com>
2021-10-26 11:16:32 -0700

    added put method for planet_id route
    Co-authored-by: Katie  <katiediaz@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -2,17 +2,6 @@ from app import db
 from app.models.planet import Planet
 from flask import Blueprint, jsonify, make_response, request
 
-# planets = [
-#     Planet(1, "mercury", "the littlest planet", "gray"),
-#     Planet(2, "venus", "the hottest planet", "maroon"),
-#     Planet(3, "earth", "the liveliest planet", "green"),
-#     Planet(4, "mars", "the reddest planet", "red"),
-#     Planet(5, "jupiter", "the biggest planet", "orange"),
-#     Planet(6, "saturn", "the ring planet", "yellow"),
-#     Planet(7, "uranus", "the most sidways planet", "purple"),
-#     Planet(8, "neptune", "the boring planet", "blue"),
-# ]
-
 planet_bp = Blueprint("planet", __name__, url_prefix="/planets")
 
 
@@ -38,25 +27,18 @@ def handle_planets():
         return make_response(f"Planet {new_planet.name} successfully created", 201)
 
 
-# localhost:5000/planets/<planet_id>
-@planet_bp.route("/<planet_id>", methods=["GET"])
+@planet_bp.route("/<planet_id>", methods=["GET", "PUT"])
 def handle_planet(planet_id):
+    planet = Planet.query.get(planet_id)
     if request.method == "GET":
-        planet = Planet.query.get(planet_id)
         return planet.to_json()
+    elif request.method == "PUT":
+        form_data = request.get_json()
 
+        planet.name = form_data["name"]
+        planet.description = form_data["description"]
+        planet.color = form_data["color"]
 
-#    planets_return = []
-#    for planet in planets:
-#        planets_return.append(planet.to_json())
-#    return jsonify(planets_return)
+        db.session.commit()
 
-
-# one_planet_bp = Blueprint("one_planet", __name__, url_prefix="/planet")
-# http://127.0.0.1:5000/planet/<planet_id>
-# @one_planet_bp.route("/<planet_id>", methods=["GET"])
-# def one_planet(planet_id):
-#    planet_id = int(planet_id)
-#    for planet in planets:
-#        if planet.id == planet_id:
-#            return planet.to_json()
+        return make_response(f"Planet #{planet.id} successfully updated")
