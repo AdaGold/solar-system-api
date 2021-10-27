b4f1c5b29c0deb55702e3fea6257678b563da28e
Faith <fkauwe@gmail.com>
2021-10-27 12:03:57 -0700

    added testing functionality and fixtures, so far one test successful

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 # creating a class instance of SQLAlchemy
 db= SQLAlchemy()
 # creating a class instance of Migrate class
 migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
+
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
     # setting a SQLAlchemy config
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # establishing connection string to the database, 5432 is the port for postgres
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
     # connects db and migrate to Flask app, using package syntax
     db.init_app(app)
     migrate.init_app(app, db)
---
+++ tests/test_routes.py
@@ -0,0 +1,8 @@
+def test_get_all_planets_with_no_records(client):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
