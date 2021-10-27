49c8b7903c4954b03d577246f1fcada8b17b5baa
syblades <86990570+syblades@users.noreply.github.com>
2021-10-27 16:19:48 -0700

    added addt'l logic for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,16 @@
-# standard pytest file that holds test configurations & common test helper functions.
+# pytest file that holds test configurations & common test helper functions
 import pytest
 from app import create_app
 from app import db
+from app.models.planet import Planet
 
 
 @pytest.fixture
 def app():
     app = create_app({"TESTING": True}) 
-    # designates that the following code should have an application context. 
+    
     with app.app_context():
         db.create_all() 
-        # The lines after this statement will run after the test using the app has been completed.
         yield app 
 
     with app.app_context():
@@ -23,16 +23,19 @@ def client(app):
 	return app.test_client()
 
 
-from app.models.planet import Planet
+@pytest.fixture
+def one_saved_planet(app):
+    planet_1 = Planet(name="Red Planet", description="The best planet in the universe!", color="Red")
 
-# @pytest.fixture
-# def two_saved_books(app):
-#     # Arrange
-#     self_help_book = Book(title="The Ultimate Self Help Book", description="This will TOTALLY change ur lyfe!")
-#     food_book = Book(title="Food Book", description="A book filled with pics of food and paragraphs that are too long to tell you how to make said food... so original.")
+    db.session.add(planet_1)
+    db.session.commit()
 
-#     db.session.add_all([self_help_book, food_book])
-#     # Alternatively, we could do
-#     # db.session.add(ocean_book)
-#     # db.session.add(mountain_book)
-#     db.session.commit()
+@pytest.fixture
+def all_saved_planets(app):
+    planet_1 = Planet(name="Red Planet", description="The best planet in the universe!", color="Red")
+    planet_2 = Planet(name="Ada Planet", description="An invisible rock in our solar system", color="Let your imagination run wild with this one.")
+
+    db.session.add_all([planet_1, planet_2])
+    db.session.commit()
+
+
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,14 @@
-def test_get_all_planets(client):
+# file holds the tests for the code in app/routes.py file
+from app.models.planet import Planet
+
+def test_get_one_planet_no_data(client):
+    # Act
+    response = client.get("/planets/1")
+
+    # Assert
+    assert response.status_code == 404
+
+def test_get_all_planets_with_no_data(client):
     # Act
     response = client.get("/planets")
     response_body = response.get_json()
@@ -6,3 +16,48 @@ def test_get_all_planets(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+
+def test_get_one_planet(client, one_saved_planet):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "Red Planet",
+        "description": "The best planet in the universe!",
+        "color": "Red"
+    }
+
+def test_get_all_planets(client, all_saved_planets):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+    planet_1 = {
+        "id": 1,
+        "name": "Red Planet",
+        "description": "The best planet in the universe!",
+        "color": "Red"
+    }
+    planet_2 = {
+        "id": 2,
+        "name": "Ada Planet",
+        "description": "An invisible rock in our solar system",
+        "color": "Let your imagination run wild with this one."
+    }
+
+    # Assert
+    assert response.status_code == 200
+    assert isinstance(response_body, list)
+    assert planet_1 in response_body
+    assert planet_2 in response_body
+
+    
+def test_create_one_planet(client):
+    response = client.post("/planets", json = ({"name": "Unique Space Rock", "description": "Super somber place in the universe.", "color": "The greyest Grey"}))
+    response_body = response.get_json()
+    assert response.status_code == 201 
+    assert response_body == f"Planet Unique Space Rock successfully created"
