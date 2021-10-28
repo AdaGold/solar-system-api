a26d2148de9c97fd047ee90b5a54c21b79eecd69
Emily Colon <emiquigs@gmail.com>
2021-10-28 22:30:18 +1300

    fix patch, add .env and planet_name_query

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,42 @@
-# from flask import Flask
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+# from dotenv import load_dotenv
+# import os
 
+db = SQLAlchemy()
+migrate = Migrate()
+# load_dotenv()
+
+def create_app(test_config = None):
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    
+    # if not test_config:
+    #     app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
+    #     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    #         "solar_system_development")
+    # else:
+    #     app.config["TESTING"] = True
+    #     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    #     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    #         "solar_system_test")
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.models.planet import Planet
+
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
+
+    return app
+
+
+
+# from flask import Flask
 
 # def create_app(test_config=None):
 #     app = Flask(__name__)
@@ -8,26 +45,3 @@
 #     app.register_blueprint(planets_bp)
 
 #     return app
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-migrate = Migrate()
-
-def create_app(test_config = None):
-    app = Flask(__name__)
-    
-    app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
-
-    db.init_app(app)
-    migrate.init_app(app, db)
-
-    from app.planet import Planet
-
-    from .routes import planets_bp
-    app.register_blueprint(planets_bp)
-
-    return app
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,94 @@
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, make_response, request
+
+planets_bp = Blueprint("planets", __name__, url_prefix = "/planets")
+
+@planets_bp.route("", methods = ["GET", "POST"])
+def all_planets():
+    if request.method == "GET":
+        # planets = Planet.query.all()
+        planet_name_query = request.args.get("planet_name")
+        if planet_name_query:
+	        planets = Planet.query.filter_by(planet_name=planet_name_query)
+        else:
+	        planets = Planet.query.all()
+            
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "planet_name": planet.planet_name,
+                "description": planet.description,
+                "density": planet.density
+            })
+        return jsonify(planets_response)
+
+    elif request.method == "POST":
+        request_body = request.get_json()
+        if "title" not in request_body or "description" not in request_body:
+            return make_response("Invalid Request", 400)
+    
+        new_planet = Planet(planet_name = request_body["planet_name"],
+                description = request_body["description"],
+                density = request_body["density"])
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify(f"Book {new_planet.planet_name} created", 201)
+
+# @planets_bp.route("/<planet_id>", methods = ["GET"])
+# def one_planet(planet_id):
+#     planet = Planet.query.get(planet_id)
+
+#     return {
+#         "id": planet.id,
+#         "planet_name": planet.planet_name,
+#         "description": planet.description,
+#         "density": planet.density
+#     }
+
+@planets_bp.route("/<planet_id>", methods=["GET", "PUT", "PATCH", "DELETE"])
+def one_planet(planet_id):
+    planet = Planet.query.get_or_404(planet_id)
+    if not planet:
+        return make_response(f"Planet {planet_id} not found", 404)
+        
+    if request.method == "GET":
+        return {
+            "id": planet.id,
+            "planet_name": planet.planet_name,
+            "description": planet.description,
+            "density": planet.density
+            }
+
+    elif request.method == "PUT":
+        request_body = request.get_json()
+        planet.planet_name = request_body['planet_name']
+        planet.description = request_body['description']
+        planet.density = request_body['density']
+        db.session.commit()
+        return jsonify(f"Planet{planet.id} successfully updated"), 200
+
+    elif request.method == "PATCH":
+        request_body = request.get_json()
+        if "planet_name" in request_body:
+            planet.planet_name = request_body['planet_name']
+        if "description" in request_body:
+            planet.description = request_body['description']
+        if "density" in request_body:
+            planet.density = request_body['density']
+        db.session.commit()
+        return jsonify(f"Planet{planet.id} successfully updated"), 200
+
+    elif request.method == "DELETE":
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify(f"Planet #{planet.id} successfully deleted"), 200
+
+    
+
+
+
 # from flask import Blueprint, jsonify
 
 # class SolarSystem:
@@ -41,80 +132,3 @@
 #                 "planet_name": planet.planet_name,
 #                 "description": planet.description
 #             }
-
-
-from app import db
-from app.planet import Planet
-from flask import Blueprint, jsonify, make_response, request
-
-planets_bp = Blueprint("planets", __name__, url_prefix = "/planets")
-
-@planets_bp.route("", methods = ["GET", "POST"])
-def all_planets():
-    if request.method == "GET":
-        planets = Planet.query.all()
-        planets_response = []
-        for planet in planets:
-            planets_response.append({
-                "id": planet.id,
-                "planet_name": planet.planet_name,
-                "description": planet.description,
-                "density": planet.density
-            })
-        return jsonify(planets_response)
-
-    elif request.method == "POST":
-        request_body = request.get_json()
-        new_planet = Planet(planet_name = request_body["planet_name"],
-                            description = request_body["description"],
-                            density = request_body["density"])
-        db.session.add(new_planet)
-        db.session.commit()
-
-        return make_response(f"Planet {new_planet.planet_name} successfully created", 201)
-
-# @planets_bp.route("/<planet_id>", methods = ["GET"])
-# def one_planet(planet_id):
-#     planet = Planet.query.get(planet_id)
-
-#     return {
-#         "id": planet.id,
-#         "planet_name": planet.planet_name,
-#         "description": planet.description,
-#         "density": planet.density
-#     }
-
-@planets_bp.route("/<planet_id>", methods=["GET", "PUT", "DELETE", "PATCH"])
-def one_planet(planet_id):
-    planet = Planet.query.get(planet_id)
-    if not planet:
-        return jsonify(f"Planet{planet_id} not found"), 404
-        
-    if request.method == "GET":
-        return {
-            "id": planet.id,
-            "planet_name": planet.planet_name,
-            "description": planet.description,
-            "density": planet.density
-            }
-
-    elif request.method == "PUT":
-        request_body = request.get_json()
-        planet.planet_name = request_body['planet_name']
-        planet.description = request_body['description']
-        planet.density = request_body['density']
-        db.session.commit()
-        return jsonify(f"Planet{planet.id} successfully updated"), 200
-
-    elif request.method == "DELETE":
-        db.session.delete(planet)
-        db.session.commit()
-        return jsonify(f"Planet #{planet.id} successfully deleted"), 200
-
-    elif request.method == "PATCH":
-        request_body = request.get_json()
-        planet.planet_name = request_body['planet_name']
-        planet.description = request_body['description']
-        planet.density = request_body['density']
-        db.session.commit()
-        return jsonify(f"Planet{planet.id} successfully updated"), 200
