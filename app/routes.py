0d8bbec68a3de3d3862db1882ddb9be0a3ec2d29
Celina <armchairgraduate@gmail.com>
2022-04-22 13:13:53 -0700

    Wave 1 Complete

--- app/routes.py
+++ app/routes.py
@@ -9,5 +9,12 @@ class Planet:
 
 planets = [
     Planet(1, "Mercury", "rocky", 1),
-    Planet(2, "Venus", "gaseous", 2)
+    Planet(2, "Venus", "gaseous", 2),
+    Planet(3, "Earth", "water", 3),
+    Planet(4, "Mars", "red", 4),
+    Planet(5, "Jupiter", "big", 5),
+    Planet(6, "Saturn", "rings", 6),
+    Planet(7, "Uranus", "butt", 7),
+    Planet(8, "Neptune", "blue", 8),
+    Planet(9, "Pluto", "dwarf", 9)
 ]
