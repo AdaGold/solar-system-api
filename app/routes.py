0d6e418047dc84e866a26dab380b12e137cba893
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-04-22 12:59:38 -0500

    Created Planet class

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,8 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description, dist_from_sun):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.dist_from_sun = dist_from_sun
