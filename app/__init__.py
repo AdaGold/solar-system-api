94416c8707e3985dd50655da0364b858b206b2e2
angelica <djyl.ahn@gmail.com>
2022-04-25 14:11:12 -0500

    Wave01 Complete

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,8 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    from . import routes
+
+    app.register_blueprint(routes.bp)
+
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from crypt import methods
-from flask import Blueprint
-import json
+from flask import Blueprint, jsonify
+
 class Planet:
     def __init__(self, id, name, color, description):
         self.id = id
@@ -8,58 +8,36 @@ class Planet:
         self.color = color
         self.description = description
 
-def to_dict(self):
-    return dict(
-            id=Planet.id,
-            name=planet.name,
-            color=planet.color,
-            personality=planet.description,)
+    def to_dict(self):
+        return dict(
+                id=self.id,
+                name=self.name,
+                color=self.color,
+                personality=self.description,)
         
-
-
-solar_bp = Blueprint("solar-system", __name__)
 
 bp = Blueprint("planets", __name__, url_prefix="/planets")
 
+@bp.route("", methods=["GET"])
 def index_planets():
-    result_list = [dict(
-            id=planet.id,
-            name=planet.name,
-            color=planet.color,
-            personality=planet.description,
-        ) for planet in index_planets]
+    result_list = [planet.to_dict() for planet in planets]
 
     return jsonify(result_list) 
 
-#@solar_bp.route("/solar-system", methods=["GET"])
 
-solar_system_list = [
-    
-    { "id": "1",
-    "name": "pluto",
-    "description": "dwarf-sized"
-    } ,
+planets = [
+    Planet(1, "Pluto", "blue", "dwarf-sized"),
+    Planet(2, "Saturn", "yellow", "2nd largest planet"),
+    Planet(3, "Jupiter", "red-orange", "largest planet")
+]
 
-    { "id" : "2",
-    "name" : "saturn",
-    "description" : "2nd largest planet"
-    },
-
-    { "id" : "3",
-    "name" : "jupiter"
-    "description" : "largest planet"
-    }
-
-    
-        ]
-
-@solar_bp.route("/<id>", methods=["GET"])
+@bp.route("/<id>", methods=["GET"])
 def get_planet_id():
     id = int(id)
 
     for planet in index_planets:
         if planet.id == id:
-            jasonify(dict(
+            jsonify(dict(
             id=planet.id,
             name=planet.name,
             color=planet.color,
