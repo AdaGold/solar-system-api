414c573a4a52ca9f45739092725ad6eb5aae81ab
angelica <djyl.ahn@gmail.com>
2022-04-29 13:08:58 -0500

    Adds db functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    from . import routes
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
 
-    app.register_blueprint(routes.bp)
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
+
+    from app.models.planet import Planet
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,46 +1,66 @@
-from crypt import methods
-from flask import Blueprint, jsonify
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, abort, make_response, request
 
-class Planet:
-    def __init__(self, id, name, color, description):
-        self.id = id
-        self.name = name
-        self.color = color
-        self.description = description
+# class Planet:
+#     def __init__(self, id, name, color, description):
+#         self.id = id
+#         self.name = name
+#         self.color = color
+#         self.description = description
 
-    def to_dict(self):
-        return dict(
-                id=self.id,
-                name=self.name,
-                color=self.color,
-                personality=self.description,)
+#     def to_dict(self):
+#         return dict(
+#                 id=self.id,
+#                 name=self.name,
+#                 color=self.color,
+#                 personality=self.description,)
         
 
-bp = Blueprint("planets", __name__, url_prefix="/planets")
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-@bp.route("", methods=["GET"])
+@planets_bp.route("", methods=["GET"])
 def index_planets():
-    result_list = [planet.to_dict() for planet in planets]
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "color": planet.color,
+                "description": planet.description
+            })
+        return jsonify(planets_response)
 
-    return jsonify(result_list) 
 
+# planets = [
+#     Planet(1, "Pluto", "blue", "dwarf-sized"),
+#     Planet(2, "Saturn", "yellow", "2nd largest planet"),
+#     Planet(3, "Jupiter", "red-orange", "largest planet")
+# ]
 
-planets = [
-    Planet(1, "Pluto", "blue", "dwarf-sized"),
-    Planet(2, "Saturn", "yellow", "2nd largest planet"),
-    Planet(3, "Jupiter", "red-orange", "largest planet")
-]
+# @planets_bp.route("/<id>", methods=["GET"])
+# def get_planet_id(id):
+#     try:
 
-@bp.route("/<id>", methods=["GET"])
-def get_planet_id(id):
-    try:
-
-        id = int(id)
+#         id = int(id)
     
-    except:
-        return "Bad data, 400"
+#     except:
+#         return "Bad data, 400"
 
-    for planet in planets:
-        if planet.id == id:
-            return planet.to_dict()
-    return "Not found", 404
+#     for planet in planets:
+#         if planet.id == id:
+#             return planet.to_dict()
+#     return "Not found", 404
+@planets_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet(name=request_body["name"],
+                    color=request_body["color"],
+                    description=request_body["description"])
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
