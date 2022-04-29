e4ae7ad51affd91e2710733625aa7bc6cbf32c5d
Celina <armchairgraduate@gmail.com>
2022-04-29 11:19:27 -0700

    added planet model

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,8 @@ def create_app(test_config=None):
     db.init_app(app)
     migrate.init_app(app, db)
 
+    from app.models.planet import Planet
+
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
     return app
---
+++ app/models/planet.py
@@ -0,0 +1,8 @@
+from app import db
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    distance_from_sun = db.Column(db.Integer, nullable=False)
+    
