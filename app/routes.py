dfe40650cc45db7392e9ffc6c9eb13f899d0e54b
shelby <faulconershelby@gmail.com>
2022-04-29 11:47:21 -0700

    added create planet endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,36 +1,52 @@
-import json
-from unicodedata import name
-from flask import Blueprint, jsonify, abort, make_response
+import json # maybe don't need 
+from unicodedata import name # maybe don't need
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.planet import Planet
 
 
-class Planet:
-    def __init__(self, id, name, description, dist_from_sun):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.dist_from_sun = dist_from_sun
+# class Planet:
+#     # def __init__(self, id, name, description, dist_from_sun):
+#     #     self.id = id
+#     #     self.name = name
+#     #     self.description = description
+#     #     self.dist_from_sun = dist_from_sun
 
-    def to_dict(self):
-        return {
-            "id" : self.id,
-            "name" : self.name,
-            "description" : self.description,
-            "distance from sun" : self.dist_from_sun
-        }
+#     def to_dict(self):
+#         return {
+#             "id" : self.id,
+#             "name" : self.name,
+#             "description" : self.description,
+#             "distance from sun" : self.dist_from_sun
+#         }
 
-planets = [
-    Planet(1, "Mercury", "rocky", 1),
-    Planet(2, "Venus", "rocky", 2),
-    Planet(3, "Earth", "water", 3),
-    Planet(4, "Mars", "red", 4),
-    Planet(5, "Jupiter", "big", 5),
-    Planet(6, "Saturn", "rings", 6),
-    Planet(7, "Uranus", "butt", 7),
-    Planet(8, "Neptune", "ice", 8),
-    Planet(9, "Pluto", "dwarf", 9)
-]
+# planets = [
+#     Planet(1, "Mercury", "rocky", 1),
+#     Planet(2, "Venus", "rocky", 2),
+#     Planet(3, "Earth", "water", 3),
+#     Planet(4, "Mars", "red", 4),
+#     Planet(5, "Jupiter", "big", 5),
+#     Planet(6, "Saturn", "rings", 6),
+#     Planet(7, "Uranus", "butt", 7),
+#     Planet(8, "Neptune", "ice", 8),
+#     Planet(9, "Pluto", "dwarf", 9)
+# ]
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods = ["POST"])
+def handle_planets():
+    request_body = request.get_json()
+    new_planet = Planet(
+        name = request_body["name"], 
+        description = request_body["description"], 
+        distance_from_sun = request_body["distance from sun"]
+        )
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f'planet {new_planet.name} successfully created!', 201)
 
 @planets_bp.route("", methods = ["GET"])
 def planet_data():
