9c957977ccc75118e6df288860a62bc34b41d449
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-04 12:57:37 -0500

    Removed old code and created scratch.py file

--- app/routes.py
+++ app/routes.py
@@ -1,36 +1,6 @@
-import json # maybe don't need 
-from unicodedata import name # maybe don't need
 from flask import Blueprint, jsonify, abort, make_response, request
 from app import db
 from app.models.planet import Planet
-
-
-# class Planet:
-#     # def __init__(self, id, name, description, dist_from_sun):
-#     #     self.id = id
-#     #     self.name = name
-#     #     self.description = description
-#     #     self.dist_from_sun = dist_from_sun
-
-#     def to_dict(self):
-#         return {
-#             "id" : self.id,
-#             "name" : self.name,
-#             "description" : self.description,
-#             "distance from sun" : self.dist_from_sun
-#         }
-
-# planets = [
-#     Planet(1, "Mercury", "rocky", 1),
-#     Planet(2, "Venus", "rocky", 2),
-#     Planet(3, "Earth", "water", 3),
-#     Planet(4, "Mars", "red", 4),
-#     Planet(5, "Jupiter", "big", 5),
-#     Planet(6, "Saturn", "rings", 6),
-#     Planet(7, "Uranus", "butt", 7),
-#     Planet(8, "Neptune", "ice", 8),
-#     Planet(9, "Pluto", "dwarf", 9)
-# ]
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
@@ -95,23 +65,6 @@ def delete_planet_by_id(planet_id):
     
     return make_response(f"Planet {planet.name} successfully deleted.")
 
-    # planet_list = []
-    # for planet in planets:
-    #     planet_list.append({
-    #     "id" : planet.id,
-    #     "name" : planet.name,
-    #     "description" : planet.description,
-    #     "distance from sun" : planet.dist_from_sun
-    #     }
-    #     )
-    # return jsonify(planet_list)
-
-# @planets_bp.route("/<planet_id>", methods = ["GET"])
-# def get_planet_by_id(planet_id):
-#     planet = validate_planet(planet_id)
-#     return planet.to_dict()
-
-
 def validate_planet(planet_id):
     try:
         planet_id = int(planet_id)
@@ -123,9 +76,3 @@ def validate_planet(planet_id):
     if not planet:
         abort(make_response({"message": f"planet {planet_id} not found"}, 404))
     return planet
-
-    # for planet in planets:
-    #     if planet.id == planet_id:
-    #         return planet
-
-
