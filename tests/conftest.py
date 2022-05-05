97077442ed8f73cf4a80db1293d7bbec669a3efb
Ashley Benson <bensona2@spu.edu>
2022-05-05 11:34:01 -0700

    add test get one book

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from app import create_app
 from app import db
 from flask.signals import request_finished
+from app import Planet
 
 
 @pytest.fixture
@@ -23,3 +24,22 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+    from app.models.book import Book
+# ...
+
+@pytest.fixture
+def two_saved_planets(app):
+    # Arrange
+    mercury = Planet(name= "Mercury",
+                    description="pretty",
+                    color= "purple")
+    saturn = Planet(name="Saturn",
+                    description="has rings",
+                    color= "brown")
+
+    db.session.add_all([mercury, saturn])
+    # Alternatively, we could do
+    # db.session.add(ocean_book)
+    # db.session.add(mountain_book)
+    db.session.commit()
---
+++ tests/test_routes.py
@@ -0,0 +1,24 @@
+
+
+
+def test_get_all_planets_with_no_records(client):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_one_planet(client, two_saved_planets):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": ,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
