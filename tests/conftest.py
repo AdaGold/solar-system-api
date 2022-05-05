2df8138d155e8b37eb9c55b1709172a8edb86214
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-05 14:06:51 -0500

    Created two_saved_planets for test configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from app import create_app
 from app import db
 from flask.signals import request_finished
+from app.models.planet import Planet
 
 
 @pytest.fixture
@@ -23,4 +24,12 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
-    
+
+@pytest.fixture
+def two_saved_planets(app):
+    # arrange
+    neptune = Planet(id=1, name="Neptune", description="ice", distance_from_sun=9)
+    earth = Planet(id=2, name="Earth", description="water", distance_from_sun=3)
+
+    db.session.add_all([neptune, earth])
+    db.session.commit()
