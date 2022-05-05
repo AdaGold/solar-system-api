de695d48bdc0a1aff6909d84838b490ea9777db6
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-05 14:07:16 -0500

    Created and passed test_get_one_planet

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,3 +8,17 @@ def test_get_all_planets_with_no_records(client):
     # assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_one_planet(client, two_saved_planets):
+    # act
+    response = client.get("planets/1")
+    response_body = response.get_json()
+
+    # assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "Neptune",
+        "description": "ice",
+        "distance_from_sun": 9
+    }
