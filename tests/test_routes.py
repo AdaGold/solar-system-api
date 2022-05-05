0509329b6135f9316ded9a3b9d785be61b78ebca
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-05 13:33:59 -0500

    Created test_get_all_planets_with_no_records

---
+++ tests/test_routes.py
@@ -0,0 +1,10 @@
+
+
+def test_get_all_planets_with_no_records(client):
+    # act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # assert
+    assert response.status_code == 200
+    assert response_body == []
