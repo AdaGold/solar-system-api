6ac2887345d87041c40c426961cfa5df75571fa9
Celina <armchairgraduate@gmail.com>
2022-05-06 12:46:48 -0500

    created get all planets test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -32,3 +32,24 @@ def test_get_one_planet_with_no_records(client):
     # assert
     assert response.status_code == 404
     assert response_body == {"message": "planet 1 not found"}
+
+
+def test_get_all_planets_return_array(client, two_saved_planets):
+    # act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # assert
+    assert response.status_code == 200
+    assert response_body == [{
+        "id": 1,
+        "name": "Neptune",
+        "description": "ice",
+        "distance_from_sun": 9
+    }, 
+        {"id": 2,
+        "name": "Earth",
+        "description": "water",
+        "distance_from_sun": 3
+        }
+    ]
