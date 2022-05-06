71ae69dd90f6ec4ab5f17796df1c1ee9370188d7
shelby <faulconershelby@gmail.com>
2022-05-06 10:31:49 -0700

    Made test for planet id not found, 404

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -22,3 +22,13 @@ def test_get_one_planet(client, two_saved_planets):
         "description": "ice",
         "distance_from_sun": 9
     }
+
+
+def test_get_one_planet_with_no_records(client):
+    # act
+    response = client.get("planets/1")
+    response_body = response.get_json()
+
+    # assert
+    assert response.status_code == 404
+    assert response_body == {"message": "planet 1 not found"}
