50c6395d3b264b51917b09b559b24f686269c5ae
kindra greenawalt <kindra.kathleen@gmail.com>
2022-10-21 13:55:53 -0400

    defined planet class and started a hard code list of of planets.

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,12 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self,id,name,description,temperature):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.temperature = temperature
+
+planets = [
+    Planet(1,"Jupiter",)
+]
