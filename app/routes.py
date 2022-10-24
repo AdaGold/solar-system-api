c8e5e95ed0c10c79ca68e479478d4f55ba09eb61
Makeesha Pruitt <makeeshalp@gmail.com>
2022-10-24 11:50:35 -0400

    added get_one_planet function

--- app/routes.py
+++ app/routes.py
@@ -15,15 +15,20 @@ planets = [
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
-@planets_bp.route("",methods=["GET"])
+@planets_bp.route("", methods=["GET"])
 def get_all_planets():
-    planets_response = []
-    for planet in planets:
-        planets_response.append({
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description
-        })
+    planets_response = [{"id": planet.id, "name": planet.name, "description": planet.description} for planet in planets]
+
     return jsonify(planets_response)
+
+@planets_bp.route('<planet_id>',methods=["GET"])    
+def get_one_planet(planet_id):
+    planet_id = int(planet_id)
+    for planet in planets:
+        if planet.id == planet_id:
+            return {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description
+            }
     
-        
