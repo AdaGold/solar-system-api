578b1f77c3cebbe187cbf51be3c2121a1aa32428
Soleil <soleilherring@gmail.com>
2022-10-24 11:35:17 -0700

    made hello in routes

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,5 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    #need to register blueprint here for planets_bp
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,22 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description, size):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.size = size
+
+
+planets = [
+    Planet(1, "Mars", "Red", 2106), 
+    Planet(2, "Earth", "Blue", 3958),
+    Planet(3, "Mercury", "Grey", 1500)
+]
+
+planet_bp = Blueprint("planets", __name__)
+
+
+#this is the decorator that saying when a request matches turn this function into url
+@planet_bp.route("/planets", method = ["GET"])
+#need to create function here 
