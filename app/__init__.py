d13796cefd967949a896a820ddfe8bbb5e811fe9
Tia Willis <tiawillis1@icloud.com>
2022-10-25 14:11:31 -0600

    Wave 1 and 2 completion.

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,11 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
+
     return app
+    
+# commands to remember: 
+# flask run -h localhost -p 3000
+# FLASK_ENV=development flask run
