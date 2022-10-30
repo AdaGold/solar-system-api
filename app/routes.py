6090479c75453dcce233fbba05263a13891da630
Jennifer <datateur@gmail.com>
2022-10-29 17:44:33 -0700

    Created endpoint to get single planet

--- app/routes.py
+++ app/routes.py
@@ -1,50 +1,8 @@
-from flask import Blueprint, jsonify, abort, make_response, request
+from flask import Blueprint, jsonify, make_response, request
 from app.models.planet import Planet
 from app import db
 
-# planets = [
-#         Planet(1, "Mercury", "The smallest and fastest planet", False),
-#         Planet(2, "Venus", "The hottest planet", False),
-#         Planet(3, "Earth", "The blue marble", False),
-#         Planet(4, "Mars", "The red planet", False),
-#         Planet(5, "Jupiter", "The gas giant", False),
-#         Planet(6, "Saturn", "The second largest planet", True),
-#         Planet(7, "Uranus", "This planet spins on its side", True),
-#         Planet(8, "Neptune", "The most distant planet from the sun", False)
-#         ]
-
 bp = Blueprint("planets", __name__, url_prefix="/planets")
-
-# @bp.route("", methods=["GET"])
-# def handle_planets():
-#     return_body = []
-#     for planet in planets:
-#         return_body.append({
-#             "id": planet.id,
-#             "name": planet.name,
-#             "description": planet.description,
-#             "rings": planet.rings
-#         })
-    
-#     return jsonify(return_body)
-
-# @bp.route("/<id>", methods=["GET"])
-# def handle_planet(id):
-#     try:
-#         planet_id = int(id)
-#     except:
-#         abort(make_response({"message":f"planet {id} is not valid"}, 400))
-    
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return jsonify({
-#                 "id": planet.id,
-#                 "name": planet.name,
-#                 "description": planet.description,
-#                 "rings": planet.rings
-#             })
-    
-#     abort(make_response({"message":f"Planet {id} not found."}, 404))
 
 @bp.route("", methods=["POST"])
 def create_planet():
@@ -58,3 +16,20 @@ def create_planet():
 
     return make_response(f"Planet {new_planet.name} created", 201)
 
+@bp.route("", methods=["GET"])
+def get_all_planets():
+    planets = Planet.query.all()
+    planets_response = []
+    for planet in planets:
+        planets_response.append(planet.build_planet_dict())
+
+    return jsonify(planets_response)
+
+@bp.route("/<id>", methods=["GET"])
+def get_planet(id):
+    planets = Planet.query.all()
+    for planet in planets:
+        if planet.id == int(id):
+            return jsonify(planet.build_planet_dict())
+    
+    return make_response(f"Planet {id} not found", 404)
