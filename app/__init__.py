dd7d5a50c0849187b85cce7d81619ef71767e713
Andrea-Galindo <andreagalindo.guerra@gmail.com>
2022-11-01 10:56:19 -0400

    Rename database

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
 
     
     db.init_app(app) 
