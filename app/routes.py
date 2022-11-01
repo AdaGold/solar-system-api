612a5ffd19d92fce7e99ac7d7ead9971a725a302
Kindra <kindra.kathleen@gmail.com>
2022-11-01 15:03:04 -0400

    created get one planet, update one planet, and delete one planet. Bugs in later two

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from attr import validate
 from app import db
 from app.models.planet import Planet
 
@@ -6,6 +7,19 @@ from flask import Blueprint , jsonify, abort, make_response, request
 
 
 planet_bp = Blueprint("planets", __name__,url_prefix="/planets")
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+    
+    planet = Planet.query.get(planet_id)
+
+    if not planet:
+        abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+    
+    return planet
 
 @planet_bp.route("", methods=["POST"])
 def create_planet():
@@ -28,11 +42,45 @@ def read_all_planets():
             {
             "id": planet.id,
             "name": planet.name,
-            "description": planet.description
+            "description": planet.description,
+            "diameter": planet.diameter
             }
         )
     return jsonify(planets_response)
+    
 
+@planet_bp.route("/<planet_id>", methods=["GET"])
+def read_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "diameter": planet.diameter
+    }
+
+@planet_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit
+
+    return make_response(jsonify(f"Planet #{planet.id} successfully deleted!"))
+
+@planet_bp.route("/<planet_id>", methods["PUT"])
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.diameter = request_body["diameter"]
+
+    db.session.commit()
+
+    return make_response(jsonify(f"Planet #{planet_id} successfully updated! "))
 
 
 
