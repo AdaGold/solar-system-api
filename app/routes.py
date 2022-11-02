8c9f174b212b1f8ac32bb3a710f0c5693fe80146
Soleil <soleilherring@gmail.com>
2022-11-02 11:50:20 -0700

    fixed GET one planet

--- app/routes.py
+++ app/routes.py
@@ -33,37 +33,42 @@ def all_planet_data():
                 "description": planet.description
             })
         return jsonify(planet_data) 
- 
+
 def validate_planet(planet_id):
     try: 
         planet_id = int(planet_id)
     except:
         abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+    
     planet = Planet.query.get(planet_id)
+    
     if not planet:
         abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+    
+    return planet
 
 @planet_bp.route("/<planet_id>", methods= ["GET"])
 def read_one_planet(planet_id):
     planet = validate_planet(planet_id)
+
     return {
                 "id": planet.id, 
                 "name": planet.name,
                 "description": planet.description
             }
 
-@planet_bp.route("/<planet_id>", methods=["PUT"])
-def update_planet(planet_id):
-    planet = validate_planet(planet_id)
+# @planet_bp.route("/<planet_id>", methods=["PUT"])
+# def update_planet(planet_id):
+#     planet = validate_planet(planet_id)
 
-    request_body = request.get_json()
+#     request_body = request.get_json()
 
-    planet.name = request_body["name"]
-    planet.description = request_body["description"]
+#     planet.name = request_body["name"]
+#     planet.description = request_body["description"]
 
-    db.session.commit()
+#     db.session.commit()
 
-    return make_response(f"Planet #{planet.id} successfully updated")
+#     return make_response(f"Planet #{planet.id} successfully updated")
 
 # class Planet:
 #     def __init__(self, id, name, description, size):
