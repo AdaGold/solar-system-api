c70bfd7891d076079bf4fa7b31a36dd801f495d3
Kindra <kindra.kathleen@gmail.com>
2022-11-03 18:17:57 -0400

    test files and a few pytests created

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from app import create_app
+from app import db
+from flask.signals import request_finished
+from app.models.planet import Planet
+
+@pytest.fixture
+def app():
+    app = create_app({"TESTING": True})
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
+    with app.app_context():
+        db.drop_all()
+
+
+
+@pytest.fixture
+def two_saved_planets(app):
+    # Arrange
+    venus_planet = Planet(name="Venus",
+                      description="a fun planet to be on",
+                      diameter = "2,374 km")
+    mars_planet = Planet(name="Mars",
+                         description="home of the martians",
+                         diameter="3,456 km")
+
+    db.session.add_all([venus_planet, mars_planet])
+    db.session.commit()
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
---
+++ tests/test_routes.py
@@ -0,0 +1,40 @@
+import pytest
+from app.models.planet import Planet
+
+def test_get_all_planets_with_no_records(client):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+
+def test_get_one_planet(client, two_saved_planets):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "Venus",
+        "description": "a fun planet to be on",
+        "diameter":"2,374 km",
+    }
+
+def test_create_one_planet(client):
+    # Act
+    response = client.post("/planets", json={
+        "name": "Saturn",
+        "description": "rings of gold shine bright",
+        "diameter": "5,467 km"
+    })
+    response_body = response.get_json()
+
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == "Planet Saturn successfully created, 201"
