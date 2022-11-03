b7b7bbeba6c886fb3d4bda5188224984e72fc0ed
Jennifer <datateur@gmail.com>
2022-11-02 18:21:26 -0700

    Created PUT route to update single planet

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,21 @@
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, abort, make_response, request
 from app.models.planet import Planet
 from app import db
 
 bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"book {planet_id} invalid"}, 400))
+
+    planet = Planet.query.get(planet_id)
+
+    if not planet:
+        abort(make_response({"message":f"book {planet_id} not found"}, 404))
+
+    return planet
 
 @bp.route("", methods=["POST"])
 def create_planet():
@@ -23,28 +36,26 @@ def get_all_planets():
     for planet in planets:
         planets_response.append(planet.build_planet_dict())
 
-    return jsonify(planets_response)
+    return jsonify(planets_response), 400
 
 @bp.route("/<id>", methods=["GET"])
-def get_planet(id):
-    planets = Planet.query.all()
-    for planet in planets:
-        if planet.id == int(id):
-            return jsonify(planet.build_planet_dict())
-    
-    return make_response(f"Planet {id} not found", 404)
+def read_one_planet(id):
+    planet = validate_planet(id)
 
-# @bp.route("/<id>", methods=["POST"])
-# def update_planet():
-#     request_body = request.get_json()
+    return jsonify(planet.build_planet_dict())
 
-#     updated_planet = Planet(title=request_body["name"],
-#                     description=request_body["description"],
-#                     rings=request_body["rings"])
+@bp.route("/<id>", methods=["PUT"])
+def update_planet(id):
+    planet = validate_planet(id)
+    request_body = request.get_json()
 
+    planet.name = request_body["name"],
+    planet.description = request_body["description"],
+    planet.rings = request_body["rings"]
 
-#     db.session.(updated_planet)
-#     db.session.commit()
+    db.session.commit()
+
+    return make_response(f"Planet #{id} successfully updated")
 
 # @bp.route("/guide/<id>", methods=["DELETE"])
 # def planet_delete(id):
