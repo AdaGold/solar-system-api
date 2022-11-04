3f8a911fcd8573a7fc1d56b683abe1c5b37a436d
Jennifer <datateur@gmail.com>
2022-11-03 19:03:58 -0700

    cleaned test files

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,13 +8,6 @@ def test_get_all_planets_with_no_records(client):
     assert response.status_code == 200
     assert response_body == []
 
-"""Create test fixtures and unit tests for the following test cases:
-
-1. `GET` `/planets/1` returns a response body that matches our fixture
-1. `GET` `/planets/1` with no data in test database (no fixture) returns a `404`
-1. `GET` `/planets` with valid test data (fixtures) returns a `200` with an array including appropriate test data
-1. `POST` `/planets` with a JSON request body returns a `201`"""
-
 def test_get_one_planet_returns_planet(client, one_saved_planet):
     response = client.get("/planets/1")
     response_body = response.get_json()
@@ -24,10 +17,6 @@ def test_get_one_planet_returns_planet(client, one_saved_planet):
     assert response_body["name"] == one_saved_planet.name
     assert response_body["rings"] == one_saved_planet.rings
     assert response_body["description"] == one_saved_planet.description
-    # assert response_body == {'id': 1, 
-    #                         'name': 'Earth', 
-    #                         'description': 'The blue marble', 
-    #                         'rings': False}
 
 def test_get_one_planet_with_no_records(client):
     response = client.get("/planets/1")
