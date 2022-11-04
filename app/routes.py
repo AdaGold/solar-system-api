a85273763b19359124b215a8feab654f47e539f6
Andrea-Galindo <andreagalindo.guerra@gmail.com>
2022-11-04 11:06:39 -0400

    added tests

--- app/routes.py
+++ app/routes.py
@@ -34,10 +34,11 @@ def read_one_planet(planet_id):
 @planets_bp.route("", methods=["GET"])
 def read_all_planets():
     name_query = request.args.get("name") 
-    if name_query: # -> if we have a query parameter, use it to filter the data. Else, retrieve all data. 
+    if name_query:  
         planets = Planet.query.filter_by(name=name_query)
     else:
         planets = Planet.query.all()
+
     planets_response = []
     planets = Planet.query.all()
     for planet in planets:
@@ -62,7 +63,7 @@ def create_planet():
     db.session.add(new_planet)
     db.session.commit()
 
-    return make_response(f"Planet {new_planet.name} successfully created", 201)
+    return make_response(jsonify(f"Planet {new_planet.name} successfully created"), 201)
 
 
 
---
+++ app/tests/conftest.py
@@ -0,0 +1,44 @@
+import pytest
+from app import create_app
+from app import db
+from flask.signals import request_finished
+from app.models.planet import Planet
+
+@pytest.fixture
+def app():
+    app = create_app({"TESTING": True})
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
+    with app.app_context():
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def two_saved_planets(app):
+    # Arrange
+    planet_mercury = Planet(name="Mercury",
+                      description="Mercury description",
+                      moons=1)
+    planet_pluto = Planet(name="Pluto",
+                         description="Pluto description",
+                         moons=2
+                         )
+
+    db.session.add_all([planet_mercury, planet_pluto])
+    # Alternatively, we could do
+    # db.session.add(ocean_book)
+    # db.session.add(mountain_book)
+    db.session.commit()
+
