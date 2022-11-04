22f547f294151dd1be385a73103147a2bc333dd2
Tia Willis <tiawillis1@icloud.com>
2022-11-03 19:08:09 -0600

    wave 6 pt1 and partial pt2 completed

---
+++ tests/conftest.py
@@ -0,0 +1,41 @@
+import pytest
+from app import create_app
+from app import db
+from flask.signals import request_finished
+
+
+@pytest.fixture
+def app():
+    app = create_app({"TESTING": True})
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
+    with app.app_context():
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+from app.models.planet import Planet
+# ...
+
+@pytest.fixture
+def two_saved_planets(app):
+    # Arrange
+    earth_planet = Planet(name="Earth",
+                    description="Inhabits life",
+                    size= "7,917.5 mi" )
+    saturn_planet = Planet(title="Saturn",
+                    description="Adorned with ringlets 0o0o0o",
+                    size="72,367 mi")
+
+    db.session.add_all([earth_planet, saturn_planet])
+    db.session.commit()
