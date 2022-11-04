78573829abd460f257583a438e8c7f24656732de
Selam Chaka <selblessed@gmail.com>
2022-11-03 22:51:04 -0400

    adding five tests passing

--- app/__init__.py
+++ app/__init__.py
@@ -12,14 +12,13 @@ def create_app(test_config=None):
     app = Flask(__name__)
     if not test_config:
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-            "SQLALCHEMY_DATABASE_URI")
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
     else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_TEST_DATABASE_URI")
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
     
+
     db.init_app(app)
     migrate.init_app(app, db)
     
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,7 +10,6 @@ def test_get_all_planets_with_no_records(client):
     assert response.status_code == 200
     assert response_body == []
 
-
 def test_get_one_planet(client, two_saved_planets):
     # Act
     response = client.get("/planets/1")
@@ -25,6 +24,37 @@ def test_get_one_planet(client, two_saved_planets):
         "diameter":"2,374 km",
     }
 
+def test_get_one_planet_with_no_records(client):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == { "message": "Planet 1 not found"}
+
+def test_get_all_planets(client, two_saved_planets):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [
+        {
+        "id": 1,
+        "name": "Venus",
+        "description": "a fun planet to be on",
+        "diameter":"2,374 km"
+        },
+        {
+        "id": 2,
+        "name": "Mars",
+        "description": "home of the martians",
+        "diameter":"3,456 km"
+        }
+        ]
+    
 def test_create_one_planet(client):
     # Act
     response = client.post("/planets", json={
