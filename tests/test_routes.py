8011fd2a5db5cb47e1bce83f78e5fe7aeb314df0
Jennifer <datateur@gmail.com>
2022-11-03 18:27:50 -0700

    Created get all planets test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -13,7 +13,7 @@ def test_get_all_planets_with_no_records(client):
 1. `GET` `/planets` with valid test data (fixtures) returns a `200` with an array including appropriate test data
 1. `POST` `/planets` with a JSON request body returns a `201`"""
 
-def test_get_one_planet(client, one_saved_planet):
+def test_get_one_planet_returns_planet(client, one_saved_planet):
     response = client.get("/planets/1")
     response_body = response.get_json()
 
@@ -22,15 +22,23 @@ def test_get_one_planet(client, one_saved_planet):
     assert response_body["name"] == one_saved_planet.name
     assert response_body["rings"] == one_saved_planet.rings
     assert response_body["description"] == one_saved_planet.description
+    # assert response_body == {'id': 1, 
+    #                         'name': 'Earth', 
+    #                         'description': 'The blue marble', 
+    #                         'rings': False}
 
-def test_no_data_returns_404(client):
+def test_get_one_planet_with_no_records(client):
     response = client.get("/planets/1")
 
     assert response.status_code == 404
 
-def test_get_all_planets(client, all_planets):
+def test_get_all_planets_returns_all_planets(client, all_planets):
     response = client.get("/planets")
     response_body = response.get_json()
     
     assert response.status_code == 200
     assert len(response_body) == 8
+
+def test_post_
+
+    assert response.status_code == 201
