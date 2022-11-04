2538aa4567f6de358f5213be7c6e06031315902a
Diana Soriano <dssori@gmail.com>
2022-11-03 21:18:27 -0400

    test_routes functions

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,7 +12,9 @@ def test_get_one_planet(client,one_saved_planet):
     response_body = response.get_json()
 
     assert response.status_code == 200
-    assert response_body == ["id"] == one_saved_planet.id
-    assert response_body == ["name"] == one_saved_planet.name
-    assert response_body == ["rings"] == one_saved_planet.rings
-    assert response_body == ["description"] == one_saved_planet.rings
+    assert response_body["id"] == one_saved_planet.id
+    assert response_body["name"] == one_saved_planet.name
+    assert response_body["rings"] == one_saved_planet.rings
+    assert response_body["description"] == one_saved_planet.description
+
+1,2,3,4,55,6,
