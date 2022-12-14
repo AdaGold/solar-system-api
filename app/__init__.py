6c7b3aa257b1e5352611f4f6291eeae12a7fd8c3
Ya-Juan Ruan <yajuan@decltype.org>
2022-12-14 11:37:23 -0800

    Add and register moons route

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    from .routes import planets_bp
+    from .routes import planets_bp, moons_bp
     app.register_blueprint(planets_bp)
+    app.register_blueprint(moons_bp)
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,33 @@
 from flask import Blueprint, jsonify
+from .moon import moons_list
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+moons_bp = Blueprint("moons", __name__, url_prefix="/moons")
 
 @planets_bp.route("", methods=["GET"])
 def display_planets():
     return "Test Planets"
+
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def display_planet(planet_id):
+    pass
+
+@moons_bp.route("", methods=["GET"])
+def get_all_moons():
+    moons_response = []
+    for moon in moons_list:
+        moons_response.append({
+            "id":moon.id,
+            "name": moon.name,
+            "description": moon.description
+        })
+    return jsonify(moons_response)
+
+
+
+    
+
+
     
 
     
