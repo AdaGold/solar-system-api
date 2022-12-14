23512bfc9b6444e9cbc8f81d82de6fde5a275ac8
maple-megan333 <maple.megan333@gmail.com>
2022-12-14 11:15:01 -0800

    moons updated

--- app/moon.py
+++ app/moon.py
@@ -2,6 +2,6 @@ class Moon:
     def __init__(self, id, name, description):
         self.id=id
         self.name=name
-        self.id=id
         self.description=description
         
+moons_list = [Moon(1, "Moon", "Earths Moon")]
