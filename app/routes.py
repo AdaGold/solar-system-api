fe2a6b1ac2a5512a0b009bcf81214612596cd2af
Jennifer Dai <jendai173@gmail.com>
2022-12-14 11:13:54 -0800

    "routes updates"

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, name, gravity,description):
+        self.id = id
+        self.name = name
+        self.gravity = gravity
+        self.description = description
+
+planets = [
+    Planet(1,"Mars","","Mars is the fourth planet from the Sun. A dusty, cold, desert world with a very thin atmosphere.")
+]       
+planets_bp = Blueprint("planets", __name__)
+
+@planets_bp.route("/planets", methods = ["GET"])
+def planet_json():
+    response_body = []
