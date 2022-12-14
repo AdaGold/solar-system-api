1f920e13439cbc93e63e9fe8adc2e3218c9bd53b
Ya-Juan Ruan <yajuan@decltype.org>
2022-12-14 11:10:29 -0800

    Add planets test route

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,10 @@
-from flask import Blueprint
-
+from flask import Blueprint, jsonify
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"])
+def display_planets():
+    return "Test Planets"
+    
+
+    
 
