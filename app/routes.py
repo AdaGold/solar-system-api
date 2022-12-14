091c4cb76c07153e789ec1ddb85617d08aa8c6ad
maple-megan333 <maple.megan333@gmail.com>
2022-12-14 11:50:52 -0800

    display all planet

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,22 @@
 from flask import Blueprint, jsonify
 from .moon import moons_list
+from .planets import Planet, solar_system
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 moons_bp = Blueprint("moons", __name__, url_prefix="/moons")
 
 @planets_bp.route("", methods=["GET"])
 def display_planets():
-    return "Test Planets"
+    planets_response = []
+    for planet in solar_system:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name, 
+            "description": planet.description,
+            "Has Rings": planet.has_rings,
+            "Moons" : [moon.name for moon in planet.moons]
+        })
+    return jsonify(planets_response)
+
 
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
