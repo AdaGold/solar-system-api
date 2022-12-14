8ce866914d9ea5c9944924a20d8037d79d0a4f4f
Jennifer Dai <jendai173@gmail.com>
2022-12-14 11:35:46 -0800

    created planets route and tested in postman

--- app/routes.py
+++ app/routes.py
@@ -10,13 +10,20 @@ class Planets:
     
 planets = [
     Planets(1, "Mercury", "The smallest planet in our solar system and nearest to the Sun.", "3.7 m/s^2"),
-    Planets(1, "Earth", "The third planet from the Sun", "9.807 m/s^2"),
-    Planets(1, "Jupiter", "The largest planet in the solar system", "24.79 m/s^2")
+    Planets(2, "Earth", "The third planet from the Sun", "9.807 m/s^2"),
+    Planets(3, "Jupiter", "The largest planet in the solar system", "24.79 m/s^2")
 ]
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
-@planets_bp.route("/planets", methods = ["GET"])
-def planet_json():
-    response_body = []
-
+@planets_bp.route("", methods = ["GET"])
+def planets_json():
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "gravity":planet.gravity
+        })
+    return jsonify(planets_response)
