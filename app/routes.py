e50ac41bf8291ba539148f993344a00bc35ecfe5
Yufei Bao <ybao1005@gmail.com>
2022-12-14 11:08:11 -0800

    Monica's update: set Planet class, Blueprint, and __init__ file

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planets: 
+    def __init__(self, id, name, description, gravity): 
+        self.id = id
+        self.name = name 
+        self.description = description 
+        self.type = type 
+    
+planets = [
+    Planets(1, "Mercury", "The smallest planet in our solar system and nearest to the Sun.", "3.7 m/s^2"),
+    Planets(1, "Earth", "The third planet from the Sun", "9.807 m/s^2"),
+    Planets(1, "Jupiter", "The largest planet in the solar system", "24.79 m/s^2")
+]
+
+planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
