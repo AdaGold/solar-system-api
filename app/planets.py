27cbb176f6c71655aa7a200587a9bd855e9eb7fd
maple-megan333 <maple.megan333@gmail.com>
2022-12-14 10:50:49 -0800

    simple planet with constructor

---
+++ app/planets.py
@@ -0,0 +1,7 @@
+class Planet:
+    def __init__(self, id, name, description, has_rings=False, moons=None):
+        self.id=id
+        self.name=name
+        self.description=description
+        self.has_rings=has_rings
+        self.moons = moons if moons else []
