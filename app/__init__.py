cfc1857931f901d7ae475f0e3f77cc32a0716a63
Jennifer Dai <jendai173@gmail.com>
2022-12-15 14:15:03 -0800

    add register

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.config['JSON_SORT_KEYS'] = False
 
     from .routes import planets_bp 
     app.register_blueprint(planets_bp) 
