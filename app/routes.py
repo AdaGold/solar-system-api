d34fd18a5091474f91d8634b27455f0bd7d8a4fa
Jennifer Dai <jendai173@gmail.com>
2022-12-14 20:44:00 -0800

    wave 2 ready for review

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 
 class Planets: 
@@ -27,3 +27,27 @@ def planets_json():
             "gravity":planet.gravity
         })
     return jsonify(planets_response)
+
+#validate planet id and response
+def validate_planet(planet_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+#Get planet info by valid id
+@planets_bp.route("/<planet id>", methods = ["GET"])
+def planets_get_by_id(planet_id):
+    planet = validate_planet(planet_id)
+    return {
+        "id": planet.id,
+        "title": planet.title,
+        "description": planet.description
+    }
+
