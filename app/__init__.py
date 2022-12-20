2d081af9c4bfb772b98482f81521bd4d6d7caeef
Cindy Truong <cindythitruong@gmail.com>
2022-12-20 10:51:01 -0800

    added database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/planet_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
