34f8f90ba41c297897d746c5afda40851b982e4b
Elaine <sosohn@gmail.com>
2022-12-20 11:01:37 -0800

    Update the GET method

--- app/routes/planets.py
+++ app/routes/planets.py
@@ -15,6 +15,20 @@ planets = [
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
+@planets_bp.route("",methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet(
+        name = request_body["name"],
+        description = request_body["description"],
+        is_rocky = request_body["is_rocky"]
+    )
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response({f"Planet {new_planet.name} successfully created"}, 201)
+
 @planets_bp.route("",methods=["GET"])
 def get_all_planets():
     planets_response = []
