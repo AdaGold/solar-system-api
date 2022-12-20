3d1d8118354111612c9fba24bfe8f7fd23f84230
nadxelle <nadxellehernandez@gmail.com>
2022-12-20 12:01:53 -0700

    Finished the GET method for all planets

--- app/routes/planets.py
+++ app/routes/planets.py
@@ -18,6 +18,7 @@ planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 @planets_bp.route("",methods=["GET"])
 def get_all_planets():
     planets_response = []
+    planets = Planet.query.all()
     for planet in planets:
         planets_response.append(planet.to_dict())
     return jsonify(planets_response)
