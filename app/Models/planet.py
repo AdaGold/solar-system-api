bad112310f1cc4250c2e40f89f26996d47ecdb04
Jennifer Dai <jendai173@gmail.com>
2022-12-21 15:58:14 -0800

    change class planets to planet in planet.py

--- app/Models/planet.py
+++ app/Models/planet.py
@@ -1,7 +1,7 @@
 from app import db
 
 
-class Planets(db.Model):
+class Planet(db.Model):
     
     id = db.Column(db.Integer,primary_key = True, autoincrement = True)
     name = db.Column(db.String)
