1a82e92fc3e2f8fb3756dd59c8a81825184a78fc
Yufei Bao <ybao1005@gmail.com>
2023-01-01 20:30:07 -0800

    testing config added

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from dotenv import load_dotenv 
+import os 
 
 #postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development
 db = SQLAlchemy()
 migrate = Migrate(compare_type=True)
+load_dotenv() 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    # app.config['JSON_SORT_KEYS'] = False # Don't sort keys alphabetically
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    app.config['JSON_SORT_KEYS'] = False # Don't sort keys alphabetically
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
-
+    if test_config: 
+        app.config['JSON_SORT_KEYS'] = False # Don't sort keys alphabetically
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+    else:
+        app.config['JSON_SORT_KEYS'] = False # Don't sort keys alphabetically
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
     db.init_app(app)
     migrate.init_app(app, db)
--- app/planet_routes.py
+++ app/planet_routes.py
@@ -52,7 +52,7 @@ def create_planet():
     db.session.add(new_planet)
     db.session.commit() 
 
-    return make_response(f"Planet: {new_planet.name} created successfully.", 201)
+    return make_response(jsonify(f"Planet: {new_planet.name} created successfully."), 201)
 
 @planets_bp.route("", methods = ["GET"])
 def read_planets(): 
@@ -108,7 +108,7 @@ def read_planets():
             "gravity": planet.gravity, 
             "distance": planet.distance}
         )
-    return jsonify(planet_response), 200 
+    return make_response(jsonify(planet_response), 200)
 
 @planets_bp.route("/<planet_id>", methods = ["GET"])
 def read_one_planet_by_id(planet_id): 
@@ -136,7 +136,7 @@ def update_planet_by_id(planet_id):
 
     db.session.commit() 
 
-    return (f"Planet: {planet_id} has been updated successfully.", 200) 
+    return make_response(jsonify(f"Planet: {planet_id} has been updated successfully."), 200) 
 
 @planets_bp.route("/<planet_id>", methods = ["DELETE"])
 def delete_planet_by_id(planet_id): 
@@ -145,73 +145,4 @@ def delete_planet_by_id(planet_id):
     db.session.delete(planet)
     db.session.commit()
 
-    return (f"Planet: {planet_id} has been deleted successfully.", 200) 
-
-
-# ----------------------- Hard coded routes for practice and notes ------------------------------
-# class Planets:
-#     def __init__(self, id, name, description, gravity):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.gravity = gravity
-
-#     def to_dict(self): 
-#         return {
-#             "id": self.id,
-#             "name": self.name,
-#             "description": self.description,
-#             "gravity": self.gravity
-#         }
-
-
-# planets = [
-#     Planets(1, "Mercury",
-#             "The smallest planet in our solar system and nearest to the Sun.", "3.7 m/s^2"),
-#     Planets(2, "Earth", "The third planet from the Sun", "9.807 m/s^2"),
-#     Planets(3, "Jupiter", "The largest planet in the solar system", "24.79 m/s^2")
-# ]
-
-# planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
-
-
-# @planets_bp.route("", methods=["GET"])
-# def planets_json():
-#     planets_response = []
-#     for planet in planets:
-#         planets_response.append({
-#             "id": planet.id,
-#             "name": planet.name,
-#             "description": planet.description,
-#             "gravity": planet.gravity
-#         })
-#     return jsonify(planets_response), 200
-
-
-# def planet_validation(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except:
-#         abort(make_response({"Message": "Planet id must be an integer."}, 401))
-
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return planet
-
-#     abort(make_response({"Message": f"Planet {planet_id} not found."}, 404))
-
-
-# @planets_bp.route("/<planet_id>", methods=["GET"])
-# def get_planet_by_id(planet_id):
-#     planet = planet_validation(planet_id)
-#     return jsonify(planet.to_dict()), 200
-
-# @planets_bp.route("/name/<planet_name>", methods=["GET"])
-# def get_planet_by_name(planet_name):
-#     for planet in planets: 
-#         if planet.name.lower() == planet_name.lower(): 
-#             return jsonify(planet.to_dict()), 200
-
-#     abort(make_response({"Message": f"Planet {planet_name} not found."}, 404))
-
-# ----------------------- Hard coded routes for practice and notes ------------------------------
+    return make_response(jsonify(f"Planet: {planet_id} has been deleted successfully."), 200) 
