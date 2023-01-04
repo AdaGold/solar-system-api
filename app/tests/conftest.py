540e821ccaecde89043df1b13ea9d38d40906cb3
Elaine <sosohn@gmail.com>
2023-01-03 17:19:35 -0800

    Add app and client fixture

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,24 +1,24 @@
-# import pytest
-# from app import create_app, db
-# from flask.signals import request_finished
+import pytest
+from app import create_app, db
+from flask.signals import request_finished
 
-# @pytest.fixture
-# def app():
-#     app = create_app(test_config=True)
+@pytest.fixture
+def app():
+    app = create_app(test_config=True)
 
-#     @request_finished.connect_via(app)
-#     def expire_session(sender, response, **extra):
-#         db.session.remove()
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
     
-#     with app.app_context():
-#         db.create_all()
-#         yield app
+    with app.app_context():
+        db.create_all()
+        yield app
     
-#     with app.app_context():
-#         db.drop_all()
+    with app.app_context():
+        db.drop_all()
 
-# @pytest.fixture
-# def client(app):
-#     return app.test_clinet()
+@pytest.fixture
+def client(app):
+    return app.test_clinet()
 
 
