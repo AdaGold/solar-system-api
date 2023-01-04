03d08f465ceba032eeeb45071a7875aa4d4d2dcc
yves <dwu415@gmail.com>
2023-01-03 21:22:42 -0800

    add .env file, update app/_init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
+
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/planet_development'
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
 
     db.init_app(app)
     migrate.init_app(app, db)
 
+    from app.models.planet import Planet
+
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
-    
-    from app.models.planet import Planet
 
     return app
