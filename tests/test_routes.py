d68420b623a30882f78be0546845c3fb40e20a2d
Ya-Juan Ruan <yajuan@decltype.org>
2023-01-04 12:18:26 -0800

    Create two global variables to reduce code duplication

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,52 @@
+from app.routes.planet import validate_planet
+
+NEPTUNE_PLANET = {
+        "id":1,
+        "name": "Neptune",
+        "description": "Named for the god of the sea because it is blue",
+        "mass": 102,
+        "diameter": 49528,
+        "density": 1638,
+        "gravity": 11.0,
+        "escape_velocity": 23.5,
+        "rotation_period": 16.1,
+        "day_length": 16.1,
+        "distance_from_sun": 4515,
+        "orbital_period": 59800,
+        "orbital_velocity": 5.4,
+        "orbital_inclination": 1.8,
+        "orbital_eccentricity": 0.01,
+        "obliquity_to_orbit": 28.3,
+        "mean_tempurature_c": -200,
+        "surface_pressure": None,
+        "global_magnetic_feild": True,
+        "img": "https://solarsystem.nasa.gov/resources/611/neptune-full-disk-view/?category=planets_neptune",
+        "has_rings": True,
+        "moons": []
+    }
+
+URANUS_PLANET = {
+        "name": "Uranus",
+        "description": "Smells bad",
+        "mass": 86.8,
+        "diameter": 51118,
+        "density": 1270,
+        "gravity": 8.7,
+        "escape_velocity": 21.3,
+        "rotation_period": -17.2,
+        "day_length": 17.2,
+        "distance_from_sun": 2867,
+        "orbital_period": 30589,
+        "orbital_velocity": 6.8,
+        "orbital_inclination": 0.8,
+        "orbital_eccentricity": 0.047,
+        "obliquity_to_orbit": 97.8,
+        "mean_tempurature_c": -195,
+        "surface_pressure": None,
+        "global_magnetic_feild": True,
+        "img": "https://solarsystem.nasa.gov/resources/605/keck-telescope-views-of-uranus/?category=planets_uranus",
+        "has_rings": True,
+}
 
 def test_get_all_planets_with_no_records(client):
     # Act
@@ -15,30 +64,7 @@ def test_get_all_planets_with_two_records(client, two_saved_planets):
 
     #Assert
     assert response.status_code == 200
-    assert response_body[0] == {
-        "id":1,
-        "name": "Neptune",
-        "description": "Named for the god of the sea because it is blue",
-        "mass": 102,
-        "diameter": 49528,
-        "density": 1638,
-        "gravity": 11.0,
-        "escape_velocity": 23.5,
-        "rotation_period": 16.1,
-        "day_length": 16.1,
-        "distance_from_sun": 4515,
-        "orbital_period": 59800,
-        "orbital_velocity": 5.4,
-        "orbital_inclination": 1.8,
-        "orbital_eccentricity": 0.01,
-        "obliquity_to_orbit": 28.3,
-        "mean_tempurature_c": -200,
-        "surface_pressure": None,
-        "global_magnetic_feild": True,
-        "img": "https://solarsystem.nasa.gov/resources/611/neptune-full-disk-view/?category=planets_neptune",
-        "has_rings": True,
-        "moons": []
-    }
+    assert response_body[0] == NEPTUNE_PLANET
     assert response_body[1]["name"] == "number two"
     
 
@@ -50,55 +76,11 @@ def test_get_one_planet(client, two_saved_planets):
 
     # Assert
     assert response.status_code == 200
-    assert response_body == {
-        "id":1,
-        "name": "Neptune",
-        "description": "Named for the god of the sea because it is blue",
-        "mass": 102,
-        "diameter": 49528,
-        "density": 1638,
-        "gravity": 11.0,
-        "escape_velocity": 23.5,
-        "rotation_period": 16.1,
-        "day_length": 16.1,
-        "distance_from_sun": 4515,
-        "orbital_period": 59800,
-        "orbital_velocity": 5.4,
-        "orbital_inclination": 1.8,
-        "orbital_eccentricity": 0.01,
-        "obliquity_to_orbit": 28.3,
-        "mean_tempurature_c": -200,
-        "surface_pressure": None,
-        "global_magnetic_feild": True,
-        "img": "https://solarsystem.nasa.gov/resources/611/neptune-full-disk-view/?category=planets_neptune",
-        "has_rings": True,
-        "moons": []
-    }
+    assert response_body == NEPTUNE_PLANET
 
 def test_create_one_planet(client):
     # Act
-    response = client.post("/planets", json={
-        "name": "Uranus",
-        "description": "Smells bad",
-        "mass": 86.8,
-        "diameter": 51118,
-        "density": 1270,
-        "gravity": 8.7,
-        "escape_velocity": 21.3,
-        "rotation_period": -17.2,
-        "day_length": 17.2,
-        "distance_from_sun": 2867,
-        "orbital_period": 30589,
-        "orbital_velocity": 6.8,
-        "orbital_inclination": 0.8,
-        "orbital_eccentricity": 0.047,
-        "obliquity_to_orbit": 97.8,
-        "mean_tempurature_c": -195,
-        "surface_pressure": None,
-        "global_magnetic_feild": True,
-        "img": "https://solarsystem.nasa.gov/resources/605/keck-telescope-views-of-uranus/?category=planets_uranus",
-        "has_rings": True,
-})
+    response = client.post("/planets", json=URANUS_PLANET)
     response_body = response.get_json()
 
     # Assert
@@ -107,28 +89,7 @@ def test_create_one_planet(client):
 
 def test_update_one_planet(client, two_saved_planets):
         # Act
-    response = client.put("/planets/2", json={
-        "name": "Uranus",
-        "description": "Smells bad",
-        "mass": 86.8,
-        "diameter": 51118,
-        "density": 1270,
-        "gravity": 8.7,
-        "escape_velocity": 21.3,
-        "rotation_period": -17.2,
-        "day_length": 17.2,
-        "distance_from_sun": 2867,
-        "orbital_period": 30589,
-        "orbital_velocity": 6.8,
-        "orbital_inclination": 0.8,
-        "orbital_eccentricity": 0.047,
-        "obliquity_to_orbit": 97.8,
-        "mean_tempurature_c": -195,
-        "surface_pressure": None,
-        "global_magnetic_feild": True,
-        "img": "https://solarsystem.nasa.gov/resources/605/keck-telescope-views-of-uranus/?category=planets_uranus",
-        "has_rings": True,
-})
+    response = client.put("/planets/2", json=URANUS_PLANET)
     #Arrange
     response_body = response.get_json()
 
@@ -317,3 +278,14 @@ def test_all_moons_by_planet(client, two_saved_planets, two_saved_moons):
     # Assert
     assert len(response_body) == 2
 
+
+def test_validate_planet(two_saved_planets):
+    #Act
+    result_planet = validate_planet(1)
+    
+    #Assert
+    for key, value in NEPTUNE_PLANET.items():
+        assert getattr(result_planet, key) == value
+
+
+    
