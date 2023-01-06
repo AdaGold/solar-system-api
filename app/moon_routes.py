9cc692bdb7cc2898f9fc71e25f378b53b78fe65c
Yufei Bao <ybao1005@gmail.com>
2023-01-05 20:00:52 -0800

    Nested routes tests added. All tests passed

--- app/moon_routes.py
+++ app/moon_routes.py
@@ -1,17 +1,16 @@
-from flask import Blueprint, jsonify, abort, make_response, request 
-from app import db 
-from app.models.moon import Moon 
-from app.models.planet import Planet 
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.moon import Moon
+from app.models.planet import Planet
 from app.planet_routes import validate_model
 
 moon_bp = Blueprint("moons_bp", __name__, url_prefix="/moons")
 
 
-
 @moon_bp.route("", methods=["POST"])
 def create_new_moon():
     request_body = request.get_json()
-    new_moon = Moon(name = request_body["name"], )
+    new_moon = Moon(name=request_body["name"], )
 
     db.session.add(new_moon)
     db.session.commit()
@@ -20,49 +19,51 @@ def create_new_moon():
 
 
 @moon_bp.route("/<moon_id>", methods=["GET"])
-def read_moon_by_id(moon_id): 
+def read_moon_by_id(moon_id):
     moon_response = validate_model(Moon, moon_id)
 
     result_dict = {
-        "id" : moon_response.id,
-        "name" : moon_response.name, 
+        "id": moon_response.id,
+        "name": moon_response.name,
     }
 
-    return make_response(jsonify(result_dict), 200) 
+    return make_response(jsonify(result_dict), 200)
+
 
 @moon_bp.route("/all", methods=["GET"])
-def read_all_moons(): 
+def read_all_moons():
     all_moon = Moon.query.all()
 
-    moons_response = [] 
-    for moon in all_moon: 
+    moons_response = []
+    for moon in all_moon:
         moons_response.append({
-        "id" : moon.id,
-        "name" : moon.name, 
-    }
-)
-    return make_response(jsonify(moons_response), 200) 
+            "id": moon.id,
+            "name": moon.name,
+        }
+        )
+    return make_response(jsonify(moons_response), 200)
 
-@moon_bp.route("/<planet_id>/moon", methods = ["POST"])
-def create_moon_to_planet_by_planet_id(planet_id): 
+
+@moon_bp.route("/<planet_id>/moon", methods=["POST"])
+def create_moon_to_planet_by_planet_id(planet_id):
     new_planet = validate_model(Planet, planet_id)
 
-    request_body = request.get_json() 
+    request_body = request.get_json()
     new_moon = Moon(
-        name = request_body["name"],
-        planet_id = new_planet.id, 
-        planet = new_planet
-        )
+        name=request_body["name"],
+        planet_id=new_planet.id,
+        planet=new_planet
+    )
 
     db.session.add(new_moon)
     db.session.commit()
 
-    return make_response(jsonify(f"Moon {new_moon.name} successfully created to planet {planet_id}"), 201)
+    return make_response(jsonify(new_moon.to_dict()), 201)
+
 
 @moon_bp.route("/<planet_id>/moons", methods=["GET"])
-def get_moons_by_planet_id(planet_id): 
+def get_moons_by_planet_id(planet_id):
     planet = validate_model(Planet, planet_id)
-    
-    response = planet.to_dict()
-    return make_response(jsonify(response),200) 
 
+    response = planet.to_dict()
+    return make_response(jsonify(response), 200)
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,8 +1,9 @@
 import pytest
 from werkzeug.exceptions import HTTPException
 from app.planet_routes import validate_model
-from app.models.planet import Planet 
+from app.models.planet import Planet
 from app.models.moon import Moon
+
 
 def test_get_planet_by_id_return_200_successful_code(client, saved_two_planets):
     response = client.get("/planets/1")
@@ -18,7 +19,8 @@ def test_get_planet_by_not_exist_id_return_404(client):
 
     assert response.status_code == 404
 
-def test_get_planet_by_invalid_planet_id_return_400_bad_request_error(client, saved_two_planets): 
+
+def test_get_planet_by_invalid_planet_id_return_400_bad_request_error(client, saved_two_planets):
     response = client.get("/planets/hello")
     response_body = response.get_json()
 
@@ -40,54 +42,59 @@ def test_get_all_planets_with_two_records_return_array_with_size_2(client, saved
 
     assert response.status_code == 200
     assert len(response_body) == 2
-    assert response_body[0]["id"] == 1 
-    assert response_body[0]["name"]== "Mars"
-    assert response_body[0]["description"] =="This is planet: Mars"
+    assert response_body[0]["id"] == 1
+    assert response_body[0]["name"] == "Mars"
+    assert response_body[0]["description"] == "This is planet: Mars"
     assert response_body[0]["gravity"] == 3.721
     assert response_body[0]["distance_from_earth"] == 60.81
-    assert response_body[1]["id"] == 2 
-    assert response_body[1]["name"]== "Jupiter"
+    assert response_body[1]["id"] == 2
+    assert response_body[1]["name"] == "Jupiter"
     assert response_body[1]["description"] == "This is planet: Jupiter"
     assert response_body[1]["gravity"] == 24.79
     assert response_body[1]["distance_from_earth"] == 467.64
 
+
 def test_create_one_planet_return_201_successfully_created(client):
     response = client.post("/planets",
-                        json={"name": "Venus",
-                                "description": "This is planet: Venus",
-                                "gravity": 9.87,
-                                "distance_from_earth": 67.685})
+                           json={"name": "Venus",
+                                 "description": "This is planet: Venus",
+                                 "gravity": 9.87,
+                                 "distance_from_earth": 67.685})
     response_body = response.get_json()
 
     assert response.status_code == 201
     assert response_body == "Planet: Venus created successfully."
 
-#####edge cases for create one planet
+# edge cases for create one planet
+
+
 def test_create_one_planet_no_name_return_400(client):
-    
+
     response = client.post("/planets",
-                        json={"description": "This is planet: Venus",
-                                "gravity": 9.87,
-                                "distance_from_earth": 67.685})
+                           json={"description": "This is planet: Venus",
+                                 "gravity": 9.87,
+                                 "distance_from_earth": 67.685})
 
     assert response.status_code == 400
 
 
 def test_create_one_planet_no_description_return_400(client):
     response = client.post("/planets",
-                    json={"name": "Mars",
-                    "gravity": 9.87,
-                    "distance_from_earth": 67.685})
+                           json={"name": "Mars",
+                                 "gravity": 9.87,
+                                 "distance_from_earth": 67.685})
 
     assert response.status_code == 400
 
+
 def test_create_one_planet_no_gravity_return_400(client):
     test_data = {"name": "Mars",
-                "description": "This is planet: Venus",
-                "distance_from_earth": 67.685
-                }
-    response = client.post("/planets", json = test_data)
-    assert response.status_code == 400 
+                 "description": "This is planet: Venus",
+                 "distance_from_earth": 67.685
+                 }
+    response = client.post("/planets", json=test_data)
+    assert response.status_code == 400
+
 
 def test_create_one_planet_with_extra_keys_return_201(client, saved_two_planets):
     test_data = {
@@ -105,16 +112,18 @@ def test_create_one_planet_with_extra_keys_return_201(client, saved_two_planets)
     assert response.status_code == 201
     assert response_body == "Planet: Venus created successfully."
 
+
 def test_put_planet_with_id_1_return_200_planet_successfully_replaced(client, saved_two_planets):
     resposne = client.put("/planets/1",
-                        json={"name": "New Planet",
+                          json={"name": "New Planet",
                                 "description": "This a New Planet",
                                 "gravity": 20.0,
                                 "distance_from_earth": 55.99})
     response_body = resposne.get_json()
 
     assert resposne.status_code == 200
-    assert response_body == "Planet: 1 has been updated successfully." 
+    assert response_body == "Planet: 1 has been updated successfully."
+
 
 def test_put_non_existing_planet_return_404_not_found_error(client, saved_two_planets):
     # with pytest.raises(HTTPException):
@@ -125,25 +134,27 @@ def test_put_non_existing_planet_return_404_not_found_error(client, saved_two_pl
     #                             "distance_from_earth": 55.99})
 
     resposne = client.put("/planets/9",
-                        json={"name": "New Planet",
+                          json={"name": "New Planet",
                                 "description": "This a New Planet",
                                 "gravity": 20.0,
                                 "distance_from_earth": 55.99})
     response_body = resposne.get_json()
 
     assert resposne.status_code == 404
-    assert response_body == {"message":"Planet 9 not found"}
+    assert response_body == {"message": "Planet 9 not found"}
+
 
 def test_put_invalid_planet__id_return_400_invalid_error(client, saved_two_planets):
     resposne = client.put("/planets/invalid_id",
-                        json={"name": "New Planet",
+                          json={"name": "New Planet",
                                 "description": "This a New Planet",
                                 "gravity": 20.0,
                                 "distance_from_earth": 55.99})
     response_body = resposne.get_json()
 
     assert resposne.status_code == 400
-    assert response_body == {"message":"Planet invalid_id is invalid"}
+    assert response_body == {"message": "Planet invalid_id is invalid"}
+
 
 def test_delete_planet_with_id_1_return_200_planet_successfully_deleted(client, saved_two_planets):
     response = client.delete("planets/1")
@@ -152,12 +163,14 @@ def test_delete_planet_with_id_1_return_200_planet_successfully_deleted(client, 
     assert response.status_code == 200
     assert response_body == "Planet: 1 has been deleted successfully."
 
+
 def test_delete_planet_with_non_exist_id_return_404_not_found_error(client, saved_two_planets):
     response = client.delete("planets/10")
     response_body = response.get_json()
 
     assert response.status_code == 404
     assert response_body == {"message": "Planet 10 not found"}
+
 
 def test_delete_planet_with_invalid_id_return_400_invalid_error(client, saved_two_planets):
     response = client.delete("planets/invalid_id")
@@ -166,31 +179,35 @@ def test_delete_planet_with_invalid_id_return_400_invalid_error(client, saved_tw
     assert response.status_code == 400
     assert response_body == {"message": "Planet invalid_id is invalid"}
 
+
 def test_validate_model(saved_two_planets):
     result_planet = validate_model(Planet, 1)
 
     assert result_planet.id == 1
     assert result_planet.name == "Mars"
     assert result_planet.description == "This is planet: Mars"
-    assert result_planet.gravity == 3.721 
+    assert result_planet.gravity == 3.721
     assert result_planet.distance_from_earth == 60.81
     assert result_planet.moons == []
 
+
 def test_validate_model_missing_record(saved_two_planets):
     # Calling `validate_model` without being invoked by a route will
-    # cause an `HTTPException` when an `abort` statement is reached 
+    # cause an `HTTPException` when an `abort` statement is reached
     with pytest.raises(HTTPException):
         result_planet = validate_model(Planet, "3")
-    
+
+
 def test_validate_model_invalid_id(saved_two_planets):
     with pytest.raises(HTTPException):
         result_planet = validate_model(Planet, "invalid_id")
 
-#---------------------------------------
-#---------------------------------------
-#---------Moon route tests--------------
-#---------------------------------------
-#---------------------------------------
+# ---------------------------------------
+# ---------------------------------------
+# ---------Moon route tests--------------
+# ---------------------------------------
+# ---------------------------------------
+
 
 def test_get_moon_by_id_return_200_successful_code(client, saved_two_moons):
     response = client.get("/moons/1")
@@ -206,7 +223,8 @@ def test_get_moon_by_not_exist_id_return_404(client):
 
     assert response.status_code == 404
 
-def test_get_moon_by_invalid_moon_id_return_400_bad_request_error(client, saved_two_moons): 
+
+def test_get_moon_by_invalid_moon_id_return_400_bad_request_error(client, saved_two_moons):
     response = client.get("/moons/hello")
     response_body = response.get_json()
 
@@ -228,46 +246,69 @@ def test_get_all_moons_with_two_records_return_array_with_size_2(client, saved_t
 
     assert response.status_code == 200
     assert len(response_body) == 2
-    assert response_body[0]["id"] == 1 
-    assert response_body[0]["name"]== "Moon1"
+    assert response_body[0]["id"] == 1
+    assert response_body[0]["name"] == "Moon1"
     assert response_body[1]["id"] == 2
-    assert response_body[1]["name"]== "Moon2"
+    assert response_body[1]["name"] == "Moon2"
 
 
 def test_create_one_moon_return_201_successfully_created(client):
     response = client.post("/moons",
-                        json={"name": "Moon3",
-                                "planet": "Jupiter",
-                                "planet_id": 2})
+                        json={"name": "Moon3"})
     response_body = response.get_json()
 
     assert response.status_code == 201
     assert response_body == "Moon Moon3 successfully created."
 
-def test_create_one_moon_under_planet_return_201_successfully_created(client,saved_two_planets):
-    response = client.post("moons/2/moon",
-                        json= {"name": "Moon3",
-                                "planet": "Jupiter",
-                                "planet_id": 2})
+def test_create_moon_to_planet_by_planet_id(client, saved_two_planets):
+    response = client.post("/moons/1/moon",
+                        json={
+                            "name": "planet1_moon"
+                        })
     response_body = response.get_json()
 
     assert response.status_code == 201
-    assert response_body == "Moon Moon3 successfully created to planet 2"
+    assert response_body["name"] == "planet1_moon"
+    assert response_body["planet_id"] == 1
+    assert response_body["planet"] == "Mars"
 
+def test_get_moons_by_planet_id_return_empty_list_of_moons(client, saved_two_planets):
+    response = client.get("moons/1/moons")
+    response_body = response.get_json()
 
-def test_validate_moon_model(saved_two_moons):
-    result_moon = validate_model(Moon, 1)
+    assert response.status_code == 200
+    assert response_body["id"] == 1 
+    assert response_body["name"] == "Mars"
+    assert response_body["description"] == "This is planet: Mars"
+    assert response_body["description"] == "This is planet: Mars"
+    assert response_body["gravity"] == 3.721
+    assert response_body["distance_from_earth"] == 60.81
+    assert response_body["moons"] == []
 
-    assert result_moon.id == 1
-    assert result_moon.name == "Moon1"
+def test_get_moons_by_planet_id_return_list_of_moons(client, saved_two_planets):
+    post_response = client.post("/moons/1/moon",
+                        json={
+                            "name": "planet1_moon"
+                        })
+    response = client.get("moons/1/moons")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body["id"] == 1 
+    assert response_body["name"] == "Mars"
+    assert response_body["description"] == "This is planet: Mars"
+    assert response_body["description"] == "This is planet: Mars"
+    assert response_body["gravity"] == 3.721
+    assert response_body["distance_from_earth"] == 60.81
+    assert response_body["moons"] == ["planet1_moon"]
 
 def test_validate_model_missing_moon_record(saved_two_moons):
     # Calling `validate_model` without being invoked by a route will
-    # cause an `HTTPException` when an `abort` statement is reached 
+    # cause an `HTTPException` when an `abort` statement is reached
     with pytest.raises(HTTPException):
         result_planet = validate_model(Moon, "3")
-    
+
+
 def test_validate_model_invalid_moon_id(saved_two_moons):
     with pytest.raises(HTTPException):
         result_planet = validate_model(Moon, "invalid_id")
-
