4f4140e8a7a5e087bd220d036289a11fc6fcb773
Nadia Mirzai <nadia.m.mirzai@gmail.com>
2023-04-23 19:13:29 -0400

    add wave_1, created function get_planet and returned the list of all planets

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
-
+    from .routes import planet_bp
+    app.register_blueprint(planet_bp)
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
+
+planets = [
+    Planet(1, "Earth", "Solid"),
+    Planet(2, "Mars", "Solid"),
+    Planet(3, "Saturn", "Gas")
+]
+
+planet_bp = Blueprint("planet_blue_print", __name__, url_prefix="/planets")
+@planet_bp.route("", methods=["GET"])
+def get_planet():
+    all_planets = []
+    for planet in planets:
+        planet_dic = {"id" : planet.id,
+                      "name" : planet.name,
+                      "description" : planet.description}
+        all_planets.append(planet_dic)
+    return jsonify(all_planets)
