5bf5eecf0b2a7a5a65e229d555e96ec62ab6f7c2
Say Ryder <lindz.ryder@gmail.com>
2023-04-25 16:24:14 -0400

    completes wave 1&2

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+
+db =SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
-
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
+    
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,54 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response
 
+class Planet:
+    def __init__(self, id, name, description, color):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.color = color
+    
+    def planet_to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "color": self.color
+        }
+
+
+planets = [
+    Planet(1,"big", "Pretty", "Purple"),
+    Planet(2,"bigg", "Round", "Orange"),
+    Planet(3,"bigger", "Lumpy", "Rainbow"),
+    Planet(4,"biggerthan","Wiggly", "Blue"),
+]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods = ["GET"])
+def handle_planets():
+    result_list = []
+    
+    for planet in planets:
+        result_list.append(planet.planet_to_dict())
+
+    return jsonify(result_list)
+
+def validate_planet(id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({"message":f"Planet {id} invaid"}, 400))
+    
+    for planet in planets:
+        if planet.id == id:
+            return planet
+        
+    abort(make_response({"message":f"Planet {id} not found"}, 404))
+
+@planets_bp.route("/<id>", methods=["GET"])
+def handle_planet(id):
+    planet = validate_planet(id)
+    return planet.planet_to_dict()
+        
+    
