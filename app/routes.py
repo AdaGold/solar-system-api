2935179350c52071997274dddd1897a30048e8ac
alyssa <tn.alyssa@gmail.com>
2023-04-28 13:27:52 -0500

    created route to create planet

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,16 @@
-from flask import Blueprint, jsonify, abort, make_response
+from app import db
+from flask import Blueprint, jsonify, abort, make_response, request
+from app.models.planet import Planet
 
-class Planet:
-    def __init__(self, id, name, description, position_from_sun):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.position_from_sun = position_from_sun 
+
+
+
+# class Planet:
+#     def __init__(self, id, name, description, position_from_sun):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.position_from_sun = position_from_sun 
 
 # planets = [
 #         Planet(1, "mercury", "smallest in our solar system", 1),
@@ -14,6 +19,21 @@ class Planet:
 #     ]
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet(
+                            name=request_body["name"],
+                            description=request_body["description"],
+                            position_from_sun=request_body["position_from_sun"]
+                        )
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+
 
 # @planets_bp.route("", methods=["GET"])
 # def get_planets():
