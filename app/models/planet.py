00165fc80bc4c42d65267a9dcee16aba3dbc8bd3
Virginia Ramos <virginia.rare@gmail.com>
2023-04-28 13:32:35 -0500

    Migration confirmed

---
+++ app/models/planet.py
@@ -0,0 +1,7 @@
+from app import db
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    distance = db.Column(db.String(255), nullable=False)
