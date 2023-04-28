09d3931921991f63e2c12af393f2cb3782979224
Say Ryder <lindz.ryder@gmail.com>
2023-04-28 13:47:51 -0400

    creates POST endpoint

---
+++ app/models/planet.py
@@ -0,0 +1,19 @@
+from app import db
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    color = db.Column(db.String)
+    # __tablename__ = "planets"
+
+    def planet_to_dict(self):
+        return {
+        "id": self.id,
+        "name": self.name,
+        "description": self.description,
+        "color": self.color }
+        
+
+    def to_string(self):
+        return f"{self.id}: {self.name} Description {self.description} "
--- app/routes.py
+++ app/routes.py
@@ -1,38 +1,52 @@
-from flask import Blueprint, jsonify, abort, make_response
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, abort, make_response, request
 
-class Planet:
-    def __init__(self, id, name, description, color):
-        self.id =id
-        self.name = name
-        self.description = description
-        self.color = color
+# class Planet:
+#     def __init__(self, id, name, description, color):
+#         self.id =id
+#         self.name = name
+#         self.description = description
+#         self.color = color
     
-    def planet_to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "color": self.color
-        }
+#     def planet_to_dict(self):
+#         return {
+#             "id": self.id,
+#             "name": self.name,
+#             "description": self.description,
+#             "color": self.color
+#         }
 
 
-planets = [
-    Planet(1,"big", "Pretty", "Purple"),
-    Planet(2,"bigg", "Round", "Orange"),
-    Planet(3,"bigger", "Lumpy", "Rainbow"),
-    Planet(4,"biggerthan","Wiggly", "Blue"),
-]
+# planets = [
+#     Planet(1,"big", "Pretty", "Purple"),
+#     Planet(2,"bigg", "Round", "Orange"),
+#     Planet(3,"bigger", "Lumpy", "Rainbow"),
+#     Planet(4,"biggerthan","Wiggly", "Blue"),
+# ]
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
-
 @planets_bp.route("", methods = ["GET"])
-def handle_planets():
-    result_list = []
-    
+def read_all_planets():
+    planets = Planet.query.all()
+    planets_response = []
     for planet in planets:
-        result_list.append(planet.planet_to_dict())
+        planets_response.append(planet.planet_to_dict())
+    return jsonify(planets_response)
 
-    return jsonify(result_list)
+@planets_bp.route("", methods = ["POST"])
+def create_planets():
+    request_body = request.get_json()
+    if "name" not in request_body or "description" not in request_body:
+        return make_response("Invalid Requesrt", 400)
+    new_planet = Planet(
+                    name=request_body["name"],
+                    description=request_body["description"],
+                    color=request_body["color"])
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
 
 def validate_planet(id):
     try:
