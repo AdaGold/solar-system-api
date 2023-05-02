b19909a0e707868ed74ff60a95d2e2ed7aea98c4
Zhuo <zhuo.parse@gmail.com>
2023-05-02 14:02:12 -0400

    wave4 completed

--- app/planet_routes.py
+++ app/planet_routes.py
@@ -4,6 +4,20 @@ from app import db
 
 planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
+# helper function
+def validate_planet_id(id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({"message": "The id {id} invalid"}, 400))
+
+    planet = Planet.query.get(id)
+    
+    if not planet:
+        abort(make_response({"message": "The id {id} not found"}, 404))
+
+    return planet
+
 #POST Planets
 @planet_bp.route("", methods=["POST"])
 def create_planet():
@@ -33,6 +47,53 @@ def get_all_planets():
             )
         )
     return jsonify(results)
+
+# GET one planet
+@planet_bp.route("/<planet_id>", methods = ["GET"])
+def get_one_planet(planet_id):
+
+    planet = validate_planet_id(planet_id)
+
+    response_body = dict(
+        id = planet.id,
+        name = planet.name, 
+        description = planet.description
+    )
+
+    return response_body
+
+# UPDATE one planet
+@planet_bp.route("/<planet_id>", methods = ["PUT"])
+def update_one_planet(planet_id):
+
+    planet = validate_planet_id(planet_id)
+
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+
+    db.session.commit()
+    
+    message = f"Planet {planet.id} successfully updated"
+
+    return make_response(message, 200)
+
+# DELETE one planet
+@planet_bp.route("/<planet_id>", methods = ["DELETE"])
+def delete_one_planet(planet_id):
+
+    planet = validate_planet_id(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    message = f"Planet {planet.id} successfully deleted"
+
+    return make_response(message, 200)
+
+
+
 
 
 # def validate_planet_id(id):
