194f5d90b8564c9c7f339fda6dbd58f3fc296f1f
Nadia Mirzai <nadia.m.mirzai@gmail.com>
2023-05-02 14:50:22 -0400

    created planet model, updated create_app function to create migration package, connected Flask to our database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     from .routes import planet_bp
     app.register_blueprint(planet_bp)
+    from app.models.planet import Planet
+
     return app
