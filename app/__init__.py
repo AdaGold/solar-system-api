67a0ce792cc98520d40aa365e7edf74d0c4459a4
Liubov Dav <lubasha0512@gmail.com>
2024-10-18 15:09:15 -0400

    cann't resolve conflict

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from app.routes.planets_routes import planet_bp
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.register_blueprint(planet_bp)
 
     return app
---
+++ app/routes/planets_routes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint
+from app.models.planets import planets
+
+
+planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
+
+@planet_bp.get("")
+def get_all_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append(dict(
+            id=planet.id,
+            name=planet.name,
+            description=planet.description    
+        ))
+        
+    return planets_response
+
+
+
+
+
+
