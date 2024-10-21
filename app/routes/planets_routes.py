6945df65e64464674703c5da8280f88ddbc6a2b7
anhtran <anhtr077@gmail.com>
2024-10-20 22:40:08 -0700

    Update get one planet and handeling errors

--- app/routes/planets_routes.py
+++ app/routes/planets_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from app.models.planets import planets
 
 planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
@@ -11,6 +11,32 @@ def get_all_planets():
             id=planet.id,
             name=planet.name,
             description=planet.description    
+            
         ))
     
     return planets_response
+
+@planet_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_palnet(planet_id)
+
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+    }
+
+
+def validate_palnet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        response = {"message": f"planet {planet_id} invalid"}
+        abort(make_response(response, 400))
+    
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    
+    response = {"message": f"planet {planet_id} not found"}
+    abort(make_response(response, 404))
