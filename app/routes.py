baebb44ff2f136c9366cad9ddd8fbcc83576d937
Ajar Duishembieva <ajduishe@gmail.com>
2024-10-20 23:33:22 -0700

    add solution to Wave 2

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response
 from .models import planets
 
 planets_bp = Blueprint('planets', __name__)
@@ -17,3 +17,28 @@ def get_planets():
         )
 
     return planets_reponse
+
+@planets_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    return {
+        "id": planet.id,
+        "name": planet.title,
+        "description": planet.description,
+        "color": planet.color
+    }
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        response = {"message": f"planet {planet_id} invalid"}
+        abort(make_response(response , 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+
+    response = {"message": f"planet {planet_id} not found"}
+    abort(make_response(response, 404))
