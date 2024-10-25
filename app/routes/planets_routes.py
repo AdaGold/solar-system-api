af930b64c8ab817103ce928d5d0eb07db02a33b7
anhtran <anhtr077@gmail.com>
2024-10-25 15:11:48 -0700

    Create get method for all planets

--- app/routes/planets_routes.py
+++ app/routes/planets_routes.py
@@ -20,6 +20,31 @@ def create_planet():
     response = new_planet.to_dict()
     return response, 201
     
+@planet_bp.get("")
+def get_all_planets():
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+    
+    response_body = [planet.to_dict() for planet in planets]
+    
+    return response_body, 200
+ 
+ 
+ 
+        
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         response = {"message": f"planet {planet_id} invalid"}
+#         abort(make_response(response, 400))
+    
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
+    
+#     response = {"message": f"planet {planet_id} not found"}
+#     abort(make_response(response, 404))       
     
     
     
@@ -35,16 +60,16 @@ def create_planet():
 #     return planet.to_dict()
 
 
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except:
-#         response = {"message": f"planet {planet_id} invalid"}
-#         abort(make_response(response, 400))
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        response = {"message": f"planet {planet_id} invalid"}
+        abort(make_response(response, 400))
     
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return planet
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
     
-#     response = {"message": f"planet {planet_id} not found"}
-#     abort(make_response(response, 404))
+    response = {"message": f"planet {planet_id} not found"}
+    abort(make_response(response, 404))
