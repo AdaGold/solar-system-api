7a86581a85d7e201624c7d447ee2a26e62ab9d2d
anhtran <anhtr077@gmail.com>
2024-10-25 11:51:47 -0700

    create base and db. Update migration and connect db Planet model generate migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from app.db import db, migrate
 from app.routes.planets_routes import planet_bp
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
     
     app.register_blueprint(planet_bp)
 
--- app/routes/planets_routes.py
+++ app/routes/planets_routes.py
@@ -1,30 +1,30 @@
 from flask import Blueprint, abort, make_response
-from app.models.planets import planets
+# from app.models.planets import planets
 
 planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
 
-@planet_bp.get("")
-def get_all_planets():
-    planets_response = [planet.to_dict() for planet in planets ]
-    return planets_response
+# @planet_bp.get("")
+# def get_all_planets():
+#     planets_response = [planet.to_dict() for planet in planets ]
+#     return planets_response
 
-@planet_bp.get("/<planet_id>")
-def get_one_planet(planet_id):
-    planet = validate_planet(planet_id)
+# @planet_bp.get("/<planet_id>")
+# def get_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
 
-    return planet.to_dict()
+#     return planet.to_dict()
 
 
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        response = {"message": f"planet {planet_id} invalid"}
-        abort(make_response(response, 400))
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         response = {"message": f"planet {planet_id} invalid"}
+#         abort(make_response(response, 400))
     
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
     
-    response = {"message": f"planet {planet_id} not found"}
-    abort(make_response(response, 404))
+#     response = {"message": f"planet {planet_id} not found"}
+#     abort(make_response(response, 404))
