69ab28b0cc4bb9a92e773987dbe2089e75c572c9
Amber <110812972+Msambere@users.noreply.github.com>
2024-10-25 19:39:50 -0500

    Complete wave_03
    Add create_new_planet() and get_all_planets() with their corresponding
    routes and HTTP methods. Additonally created get_one_planet()

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,32 +1,46 @@
-from flask import Blueprint, abort, make_response
-# from app.models.planet import Planet
+from flask import Blueprint, abort, make_response, request
+from app.models.planet import Planet
+from app.db import db
+from sqlalchemy import select
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix=("/planets"))
 
 
-# @planets_bp.get("")
-# def get_all_planets():
-#     planet_list = []
-#     for planet in Planets:
-#         planet_list.append(planet.to_dict())
-#     return planet_list
+@planets_bp.post("")
+def create_planet():
+    request_body = request.get_json()
+    name = request_body['name']
+    description = request_body['description']
 
+    new_planet = Planet(name=name, description=description)
+    db.session.add(new_planet)
+    db.session.commit()
 
-# @planets_bp.get("/<planet_id>")
-# def get_one_planet(planet_id):
-#     planet = validate_planet(planet_id)
+    response_body = new_planet.to_dict()
+
+    return response_body, 201
+
+@planets_bp.get("")
+def get_all_planets():
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+
+    response_body=[]
+    for planet in planets:
+        response_body.append(planet.to_dict())
     
-#     return planet.to_dict(), 200
+    return response_body, 200
 
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except:
-#         abort(make_response({"message": f"{planet_id} is not a invalid id"}, 400))
-    
-#     for planet in Planets:
-#         if planet.id == planet_id:
-#             return planet
-#     abort(make_response({"msg": "Planet id not found"}, 404))
-        
+@planets_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    query = db.select(Planet).where(Planet.id == planet_id)
+    planet = db.session.scalar(query)
+
+    return planet.to_dict(), 200
+
+
+# Helper Functions
+
+
+
 
