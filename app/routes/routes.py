6ba35be6f25e3f67f57dc3e9bc491a4e9a328de5
Weixi He <heweixiguge@gmai.com>
2024-10-26 22:08:27 -0400

    try to do Wave3

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,42 +1,43 @@
 from flask import Blueprint, abort, make_response
-from ..models.planet import planets
+from ..models import planet
+from ..db import db
 
 solar_system_bp = Blueprint("solar_system_bp", __name__, url_prefix="/planets")
 
-@solar_system_bp.get("")
-def get_all_planets():
-    results_list = []
-    for planet in planets:
-        results_list.append(dict(
-            id=planet.id,
-            name=planet.name,
-            description=planet.description,
-            flag=planet.flag
-        ))
+# @solar_system_bp.get("")
+# def get_all_planets():
+#     results_list = []
+#     for planet in planets:
+#         results_list.append(dict(
+#             id=planet.id,
+#             name=planet.name,
+#             description=planet.description,
+#             flag=planet.flag
+#         ))
 
-    return results_list
+#     return results_list
 
-@solar_system_bp.get("/<planet_id>")
-def get_one_planet(planet_id):
-    planet = validate_planet(planet_id)
+# @solar_system_bp.get("/<planet_id>")
+# def get_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
 
-    return{
-        "id":planet.id,
-        "name":planet.name,
-        "description":planet.description,
-        "flag":planet.flag
-    }
+#     return{
+#         "id":planet.id,
+#         "name":planet.name,
+#         "description":planet.description,
+#         "flag":planet.flag
+#     }
         
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        response = {"message": f"planet {planet_id} invalid"}
-        abort(make_response(response, 400))
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         response = {"message": f"planet {planet_id} invalid"}
+#         abort(make_response(response, 400))
 
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
 
-    response = {"message": f"planet {planet_id} not found"}
-    abort(make_response(response, 404))
+#     response = {"message": f"planet {planet_id} not found"}
+#     abort(make_response(response, 404))
