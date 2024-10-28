517a1e2f13df4da222471fd8d96a604c4986be11
Sarah Koch <sarahkoch810@gmail.com>
2024-10-28 16:21:12 -0700

    Updated code to comply with PEP8 standards. Changed import statements to use absolute imports. Reorganized import statements, grouped by source and sorted alphabetically within groups. Removed old, unused code. Ensured consistency in the use of single and double quotes in strings.

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,30 +1,8 @@
 from flask import Blueprint, abort, make_response, request
-from ..models.planet import Planet
-from ..db import db 
+from app.db import db 
+from app.models.planet import Planet
 
-planets_bp = Blueprint('planets_bp', __name__, url_prefix='/planets')
-
-# @planets_bp.get('')
-# def get_all_planets():
-#     return [planet.to_dict() for planet in planets]
-
-# @planets_bp.get('/<planet_id>')
-# def get_one_planet(planet_id):
-#     planet = validate_planet(planet_id)
-
-#     return planet.to_dict()
-
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except ValueError:
-#         abort(make_response({"message": f"planet {planet_id} invalid"}, 400))
-
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return planet
-        
-#     abort(make_response({"message": f"planet {planet_id} not found"}, 404))
+planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
 @planets_bp.post("")
 def create_planet():
@@ -47,7 +25,7 @@ def get_all_planets():
 
     return [planet.to_dict() for planet in planets]
 
-@planets_bp.get('/<planet_id>')
+@planets_bp.get("/<planet_id>")
 def get_one_planet(planet_id):
     return validate_planet(planet_id).to_dict()
 
@@ -56,12 +34,6 @@ def validate_planet(planet_id):
         planet_id = int(planet_id)
     except ValueError:
         abort(make_response({"message": f"planet {planet_id} invalid"}, 400))
-
-    # for planet in planets:
-    #     if planet.id == planet_id:
-    #         return planet
-        
-    # abort(make_response({"message": f"planet {planet_id} not found"}, 404))
 
     planet = db.session.get(Planet, planet_id)
     if planet is None:
