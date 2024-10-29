d4231fdb3f6ab8312d511a3fc82cd4adbad7b1b6
Madina-j <dzhetegenova@gmail.com>
2024-10-29 11:49:16 -0700

    Creat get planet and get planets

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,9 +1,43 @@
-from flask import Blueprint, abort, make_response
-from ..models import planet
+from flask import Blueprint, abort, make_response, request
+from ..models.planet import Planet
 from ..db import db
 
 solar_system_bp = Blueprint("solar_system_bp", __name__, url_prefix="/planets")
 
+@solar_system_bp.post("")
+def create_planet():
+    request_body = request.get_json()
+    name = request_body["name"]
+    description = request_body["description"]
+    flag = request_body["flag"]
+
+    new_planet = Planet(name=name, description=description, flag=flag)
+    db.session.add(new_planet)
+    db.session.commit()
+
+    response = {
+        "id": new_planet.id,
+        "name": new_planet.name,
+        "description": new_planet.description,
+        "flag": new_planet.flag
+    }
+    return response, 201
+@solar_system_bp.get("")
+def get_all_planets():
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+
+    planet_response = []
+    for planet in planets:
+        planet_response.append(
+            {
+                "id": planet.id,
+                "name": planet.title,
+                "description": planet.description,
+                "flag": planet.flag
+            }
+        )
+    return planet_response
 # @solar_system_bp.get("")
 # def get_all_planets():
 #     results_list = []
