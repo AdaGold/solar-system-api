ad1fa323d35e9b0ed9537349798b63d38c380ac1
Corntto9 <>
2024-10-29 11:23:12 -0700

    complete wave 4

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.planet import Planet
 from app.db import db
 from sqlalchemy import select
@@ -33,14 +33,44 @@ def get_all_planets():
 
 @planets_bp.get("/<planet_id>")
 def get_one_planet(planet_id):
-    query = db.select(Planet).where(Planet.id == planet_id)
-    planet = db.session.scalar(query)
+    planet = validate_planet_id(planet_id)
 
     return planet.to_dict(), 200
 
 
 # Helper Functions
+def validate_planet_id(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        response = {"msg": f"Planet id {planet_id} is invalid."}
+        abort(make_response(response,400))
+    
+    query = db.select(Planet).where(Planet.id == planet_id)
+    planet = db.session.scalar(query)
 
+    if not planet:
+        response = {"msg": f"Planet id {planet_id} not found."}
+        abort(make_response(response,404))
 
+    return planet
 
+@planets_bp.put("/<planet_id>")
+def update_planet(planet_id):
+    planet = validate_planet_id(planet_id)
+    request_body = request.get_json()
+
+    planet.name = request_body['name']
+    planet.description = request_body['description']
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+@planets_bp.delete("/<planet_id>")
+def delete_planet(planet_id):
+    planet = validate_planet_id(planet_id)
+    db.session.delete(planet)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
 
