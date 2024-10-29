1a2fd507905ed4132f0fa933ea829a046b791b5b
anhtran <anhtr077@gmail.com>
2024-10-29 11:05:46 -0700

    remove id in POST method

--- app/routes/planets_routes.py
+++ app/routes/planets_routes.py
@@ -7,13 +7,13 @@ planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
 @planet_bp.post("")
 def create_planet():
     request_body = request.get_json()
-    id = request_body["id"]
+    
     name = request_body["name"]
     description = request_body["description"]
     diameter = request_body["diameter"]
     number_of_moons = request_body["number_of_moons"]
     
-    new_planet = Planet(id=id, name=name, description=description, diameter=diameter, number_of_moons=number_of_moons)
+    new_planet = Planet(name=name, description=description, diameter=diameter, number_of_moons=number_of_moons)
     db.session.add(new_planet)
     db.session.commit()
     
@@ -32,29 +32,3 @@ def get_all_planets():
  
  
         
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except:
-#         response = {"message": f"planet {planet_id} invalid"}
-#         abort(make_response(response, 400))
-    
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return planet
-    
-#     response = {"message": f"planet {planet_id} not found"}
-#     abort(make_response(response, 404))       
-    
-    
-    
-# @planet_bp.get("")
-# def get_all_planets():
-#     planets_response = [planet.to_dict() for planet in planets ]
-#     return planets_response
-
-# @planet_bp.get("/<planet_id>")
-# def get_one_planet(planet_id):
-#     planet = validate_planet(planet_id)
-
-#     return planet.to_dict()
