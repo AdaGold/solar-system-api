babd8378064d900d9566952748d01694db740f5a
Sarah Koch <sarahkoch810@gmail.com>
2024-10-29 12:01:48 -0700

    Updated validation to access database. Added functionality to update and delete a planet.
    Co-authored-by: Pri <PriyankaKaramchandani@users.noreply.github.com>
    Co-authored-by: Charday Neal <chardayneal@users.noreply.github.com>

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.db import db 
 from app.models.planet import Planet
 
@@ -29,14 +29,38 @@ def get_all_planets():
 def get_one_planet(planet_id):
     return validate_planet(planet_id).to_dict()
 
+@planets_bp.put("/<planet_id>")
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.number_of_moons = request_body["number_of_moons"]
+
+    db.session.commit()
+
+    return Response(status=204, mimetype='application/json')
+
+@planets_bp.delete("/<planet_id>")
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return Response(status=204, mimetype='application/json')
+
 def validate_planet(planet_id):
     try:
         planet_id = int(planet_id)
     except ValueError:
         abort(make_response({"message": f"planet {planet_id} invalid"}, 400))
 
-    planet = db.session.get(Planet, planet_id)
-    if planet is None:
+    query = db.select(Planet).where(Planet.id == planet_id)
+    planet = db.session.scalar(query)
+
+    if not planet:
         abort(make_response({"message": f"planet {planet_id} not found"}, 404))
     
     return planet
