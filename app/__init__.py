dd74ea235e16b368ceec82d461995f90b5cd1d9c
Priyanka <pri.ppk@gmail.com>
2024-10-31 12:27:14 -0700

    Add app/__init__.py test_database configuration.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from app.db import db, migrate
 from app.routes.planet_routes import planets_bp
+from app.models import planet
+import os
 
-def create_app(test_config=None):
+def create_app(config=None):
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
+    if config:
+        app.config.update(config)
 
     db.init_app(app)
     migrate.init_app(app, db)
